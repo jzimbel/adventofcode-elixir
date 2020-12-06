41c041efc38b6c0057d6370975c0e367b3a6c419
Jon Zimbel <jzimbel@gmail.com>
2020-12-06 14:58:35 -0500

    Clean up day 4 solution

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,17 +1,17 @@
 defmodule AdventOfCode.Day04 do
-  @required_fields MapSet.new(~w[byr iyr eyr hgt hcl ecl pid])
+  @required_keys MapSet.new(~w[byr iyr eyr hgt hcl ecl pid])
 
   def part1(args) do
     args
     |> parse_pass_data()
-    |> Enum.map(&parse_pass_fields/1)
+    |> Enum.map(&parse_pass_keys/1)
     |> Enum.count(&pass_valid_p1?/1)
   end
 
   def part2(args) do
     args
     |> parse_pass_data()
-    |> Enum.map(&parse_pass_values/1)
+    |> Enum.map(&parse_pass_entries/1)
     |> Enum.count(&pass_valid_p2?/1)
   end
 
@@ -21,31 +21,33 @@ defmodule AdventOfCode.Day04 do
     |> String.split("\n\n")
   end
 
-  defp parse_pass_fields(pass_data) do
+  ### Part 1 functions
+
+  defp parse_pass_keys(pass_data) do
     ~r|(\w+):|
     |> Regex.scan(pass_data, capture: :all_but_first)
     |> Enum.flat_map(&Function.identity/1)
     |> Enum.into(MapSet.new())
   end
 
-  defp pass_valid_p1?(pass_fields) do
-    MapSet.subset?(@required_fields, pass_fields)
+  defp pass_valid_p1?(pass_keys) do
+    MapSet.subset?(@required_keys, pass_keys)
   end
 
-  defp parse_pass_values(pass_data) do
-    ~r|(\w+):([^\s]+)|
-    |> Regex.scan(pass_data, capture: :all_but_first)
-    |> Enum.into(%{}, fn [k, v] -> {k, v} end)
+  ### Part 2 functions
+
+  defp parse_pass_entries(pass_data) do
+    Regex.scan(~r|(\w+):([^\s]+)|, pass_data, capture: :all_but_first)
   end
 
-  defp pass_valid_p2?(pass_values) do
+  defp pass_valid_p2?(pass_entries) do
     valid_p1? =
-      pass_values
-      |> Map.keys()
+      pass_entries
+      |> Enum.map(fn [k | _] -> k end)
       |> MapSet.new()
       |> pass_valid_p1?()
 
-    valid_p1? and Enum.all?(pass_values, &valid_value?/1)
+    valid_p1? and Enum.all?(pass_entries, &valid_entry?/1)
   end
 
   @byr_range 1920..2002
@@ -55,44 +57,41 @@ defmodule AdventOfCode.Day04 do
   @hgt_in_range 59..76
   @ecl_options MapSet.new(~w[amb blu brn gry grn hzl oth])
 
-  defp valid_value?({"byr", val}) do
+  defp valid_entry?(["byr", val]) do
     String.to_integer(val) in @byr_range
   end
 
-  defp valid_value?({"iyr", val}) do
+  defp valid_entry?(["iyr", val]) do
     String.to_integer(val) in @iyr_range
   end
 
-  defp valid_value?({"eyr", val}) do
+  defp valid_entry?(["eyr", val]) do
     String.to_integer(val) in @eyr_range
   end
 
-  defp valid_value?({"hgt", val}) do
-    with {:cm, nil} <- {:cm, Regex.run(~r|^(\d+)cm$|, val, capture: :all_but_first)},
-         {:in, nil} <- {:in, Regex.run(~r|^(\d+)in$|, val, capture: :all_but_first)} do
-      false
-    else
-      {:cm, [height]} ->
-        String.to_integer(height) in @hgt_cm_range
-
-      {:in, [height]} ->
-        String.to_integer(height) in @hgt_in_range
+  defp valid_entry?(["hgt", val]) do
+    ~r/^(\d+)(cm|in)$/
+    |> Regex.run(val, capture: :all_but_first)
+    |> case do
+      [height, "cm"] -> String.to_integer(height) in @hgt_cm_range
+      [height, "in"] -> String.to_integer(height) in @hgt_in_range
+      nil -> false
     end
   end
 
-  defp valid_value?({"hcl", val}) do
+  defp valid_entry?(["hcl", val]) do
     Regex.match?(~r|^#[0-9a-f]{6}$|, val)
   end
 
-  defp valid_value?({"ecl", val}) do
+  defp valid_entry?(["ecl", val]) do
     val in @ecl_options
   end
 
-  defp valid_value?({"pid", val}) do
+  defp valid_entry?(["pid", val]) do
     Regex.match?(~r|^\d{9}$|, val)
   end
 
-  defp valid_value?({"cid", _val}) do
+  defp valid_entry?(["cid", _val]) do
     true
   end
 end
