d63a2c05cea3a4c6457a71ee4aa2273861356cb7
Jon Zimbel <jzimbel@gmail.com>
2020-12-05 19:24:32 -0500

    Day 1 solution

--- test/advent_of_code/day_01_test.exs
+++ test/advent_of_code/day_01_test.exs
@@ -3,19 +3,28 @@ defmodule AdventOfCode.Day01Test do
 
   import AdventOfCode.Day01
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
-
-    assert result
+  setup_all do
+    [
+      input: """
+      1721
+      979
+      366
+      299
+      675
+      1456
+      """
+    ]
   end
 
-  @tag :skip
-  test "part2" do
-    input = nil
+  test "part1", %{input: input} do
+    result = part1(input)
+
+    assert result == 514_579
+  end
+
+  test "part2", %{input: input} do
     result = part2(input)
 
-    assert result
+    assert result == 241_861_950
   end
 end
