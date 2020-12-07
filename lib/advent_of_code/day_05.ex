26ec9a58992418e9c0f97051bd319e8bfbd96eb0
Jon Zimbel <jzimbel@gmail.com>
2020-12-06 20:08:46 -0500

    Clean up day 5 solution

--- lib/advent_of_code/day_05.ex
+++ lib/advent_of_code/day_05.ex
@@ -16,36 +16,35 @@ defmodule AdventOfCode.Day05 do
     |> String.split()
     |> Enum.map(&String.split_at(&1, 7))
     |> Enum.map(fn {row, col} ->
-      {parse_partition(row, &row_mapper/1), parse_partition(col, &col_mapper/1)}
+      {parse_partition(row), parse_partition(col)}
     end)
     |> Enum.map(&seat_id/1)
   end
 
-  defp row_mapper(?F), do: ?0
-  defp row_mapper(?B), do: ?1
-
-  defp col_mapper(?L), do: ?0
-  defp col_mapper(?R), do: ?1
-
-  defp parse_partition(partition, char_mapper) do
+  defp parse_partition(partition) do
     partition
     |> String.to_charlist()
-    |> Enum.map(char_mapper)
+    |> Enum.map(&char_mapper/1)
     |> List.to_string()
     |> String.to_integer(2)
   end
+
+  defp char_mapper(?F), do: ?0
+  defp char_mapper(?B), do: ?1
+  defp char_mapper(?L), do: ?0
+  defp char_mapper(?R), do: ?1
 
   defp seat_id({row_num, col_num}), do: row_num * 8 + col_num
 
   defp find_my_seat_id(seat_ids) do
     [first_id | rest] = Enum.sort(seat_ids)
 
-    Enum.reduce_while(rest, first_id, fn seat_id, prev_seat_id ->
-      if seat_id - prev_seat_id == 2 do
+    Enum.reduce_while(rest, first_id, fn
+      seat_id, prev_seat_id when seat_id - prev_seat_id == 2 ->
         {:halt, seat_id - 1}
-      else
+
+      seat_id, _ ->
         {:cont, seat_id}
-      end
     end)
   end
 end
