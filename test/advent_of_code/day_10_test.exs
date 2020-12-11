8b652ed6b3550f60596e87f4ce5cd59230e7ef26
Jon Zimbel <jzimbel@gmail.com>
2020-12-10 22:36:56 -0500

    Day 10 solution

--- test/advent_of_code/day_10_test.exs
+++ test/advent_of_code/day_10_test.exs
@@ -3,19 +3,70 @@ defmodule AdventOfCode.Day10Test do
 
   import AdventOfCode.Day10
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
-
-    assert result
+  setup_all do
+    [
+      input1: """
+      16
+      10
+      15
+      5
+      1
+      11
+      7
+      19
+      6
+      12
+      4
+      """,
+      input2: """
+      28
+      33
+      18
+      42
+      31
+      14
+      46
+      20
+      48
+      47
+      24
+      23
+      49
+      45
+      19
+      38
+      39
+      11
+      1
+      32
+      25
+      35
+      8
+      17
+      7
+      9
+      4
+      2
+      34
+      10
+      3
+      """
+    ]
   end
 
-  @tag :skip
-  test "part2" do
-    input = nil
-    result = part2(input)
+  test "part1", %{input1: input1, input2: input2} do
+    result1 = part1(input1)
+    assert result1 == 7 * 5
 
-    assert result
+    result2 = part1(input2)
+    assert result2 == 22 * 10
+  end
+
+  test "part2", %{input1: input1, input2: input2} do
+    result1 = part2(input1)
+    assert result1 == 8
+
+    result2 = part2(input2)
+    assert result2 == 19208
   end
 end
