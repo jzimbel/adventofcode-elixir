f62eb4ce515124df207543883d638b7839e7c9ba
Jon Zimbel <jzimbel@gmail.com>
2020-12-12 17:27:37 -0500

    Help I can’t stop refactoring

--- lib/advent_of_code/day_12.ex
+++ lib/advent_of_code/day_12.ex
@@ -1,14 +1,15 @@
 defmodule AdventOfCode.Day12 do
   defmodule Ship do
-    @enforce_keys [:nav]
+    @enforce_keys ~w[cursor nav_mod]a
     defstruct posn: {0, 0},
-              nav: nil
+              cursor: nil,
+              nav_mod: nil
 
     def navigate(%Ship{} = t, []), do: t
 
-    def navigate(%Ship{nav: %nav_mod{}} = t, [action | actions]) do
+    def navigate(%Ship{} = t, [action | actions]) do
       t
-      |> nav_mod.do_action(action)
+      |> t.nav_mod.do_action(action)
       |> navigate(actions)
     end
   end
@@ -16,55 +17,44 @@ defmodule AdventOfCode.Day12 do
   defmodule Nav1 do
     import AdventOfCode.Point
 
-    defstruct heading: {1, 0}
-
-    def do_action(ship, <<dir, mag::binary>>) when dir in 'NESW' do
-      posn = translate(ship.posn, dir, String.to_integer(mag))
-      %{ship | posn: posn}
-    end
-
     def do_action(ship, <<rot_dir, deg::binary>>) when rot_dir in 'LR' do
-      heading = rotate(ship.nav.heading, String.to_integer(deg), rot_dir)
-      %{ship | nav: %{ship.nav | heading: heading}}
+      %{ship | cursor: rotate(ship.cursor, String.to_integer(deg), rot_dir)}
     end
 
     def do_action(ship, <<?F, mag::binary>>) do
-      posn = translate(ship.posn, ship.nav.heading, String.to_integer(mag))
-      %{ship | posn: posn}
+      %{ship | posn: translate(ship.posn, ship.cursor, String.to_integer(mag))}
+    end
+
+    def do_action(ship, <<dir, mag::binary>>) when dir in 'NESW' do
+      %{ship | posn: translate(ship.posn, dir, String.to_integer(mag))}
     end
   end
 
   defmodule Nav2 do
     import AdventOfCode.Point
 
-    defstruct waypoint: {10, -1}
-
     def do_action(ship, <<dir, mag::binary>>) when dir in 'NESW' do
-      waypoint = translate(ship.nav.waypoint, dir, String.to_integer(mag))
-      %{ship | nav: %{ship.nav | waypoint: waypoint}}
+      %{ship | cursor: translate(ship.cursor, dir, String.to_integer(mag))}
     end
 
-    def do_action(ship, <<rot_dir, deg::binary>>) when rot_dir in 'LR' do
-      waypoint = rotate(ship.nav.waypoint, String.to_integer(deg), rot_dir)
-      %{ship | nav: %{ship.nav | waypoint: waypoint}}
-    end
-
-    def do_action(ship, <<?F, mag::binary>>) do
-      posn = translate(ship.posn, ship.nav.waypoint, String.to_integer(mag))
-      %{ship | posn: posn}
+    def do_action(ship, action) do
+      Nav1.do_action(ship, action)
     end
   end
 
   def part1(args) do
-    ship = Ship.navigate(%Ship{nav: %Nav1{}}, String.split(args))
-
-    manhattan({0, 0}, ship.posn)
+    get_final_posn(%Ship{cursor: {1, 0}, nav_mod: Nav1}, args)
   end
 
   def part2(args) do
-    ship = Ship.navigate(%Ship{nav: %Nav2{}}, String.split(args))
+    get_final_posn(%Ship{cursor: {10, -1}, nav_mod: Nav2}, args)
+  end
 
-    manhattan({0, 0}, ship.posn)
+  defp get_final_posn(ship, input) do
+    ship
+    |> Ship.navigate(String.split(input))
+    |> Map.get(:posn)
+    |> manhattan({0, 0})
   end
 
   defp manhattan({x1, y1}, {x2, y2}), do: abs(x2 - x1) + abs(y2 - y1)
