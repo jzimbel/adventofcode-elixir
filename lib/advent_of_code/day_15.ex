003c16b7155e34994488db78dd0626d5094ca73d
Jon Zimbel <jzimbel@gmail.com>
2020-12-21 19:31:30 -0500

    Day 15 solution

--- lib/advent_of_code/day_15.ex
+++ lib/advent_of_code/day_15.ex
@@ -1,7 +1,52 @@
 defmodule AdventOfCode.Day15 do
   def part1(args) do
+    args
+    |> parse_starting_numbers()
+    |> stream_turns()
+    |> Enum.at(2020)
+    |> elem(1)
   end
 
+  # (takes about 35 sec)
   def part2(args) do
+    args
+    |> parse_starting_numbers()
+    |> stream_turns()
+    |> Enum.at(30_000_000)
+    |> elem(1)
+  end
+
+  defp parse_starting_numbers(input) do
+    input
+    |> String.trim_trailing()
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  defp stream_turns(starting_numbers) do
+    [last_number | starting_history] = Enum.reverse(starting_numbers)
+
+    history =
+      starting_history
+      |> Enum.reverse()
+      |> Enum.with_index(1)
+      |> Enum.into(%{})
+
+    turn = map_size(history) + 2
+
+    Stream.concat(
+      starting_numbers,
+      Stream.iterate({history, last_number, turn}, &next_number/1)
+    )
+  end
+
+  defp next_number({history, last_number, turn}) do
+    next_number =
+      case Map.fetch(history, last_number) do
+        {:ok, prev_turn} -> turn - 1 - prev_turn
+        :error -> 0
+      end
+
+    {Map.put(history, last_number, turn - 1), next_number, turn + 1}
   end
 end
