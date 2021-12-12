ee20039f8b2bc3ad31600ca24584eedf6f31fb4b
Jon Zimbel <jzimbel@gmail.com>
2021-12-12 14:14:36 -0500

    Add more functions to CharGrid module

--- lib/advent_of_code/char_grid.ex
+++ lib/advent_of_code/char_grid.ex
@@ -1,5 +1,5 @@
 defmodule AdventOfCode.CharGrid do
-  @moduledoc "Data structure representing a grid of characters by a map of {x, y} => char"
+  @moduledoc "Data structure representing a finite grid of characters by a map of {x, y} => char"
 
   alias __MODULE__, as: T
 
@@ -13,11 +13,25 @@ defmodule AdventOfCode.CharGrid do
 
   @type coordinates :: {non_neg_integer, non_neg_integer}
 
+  @type cell :: {coordinates, char}
+
   @enforce_keys ~w[grid width height]a
   defstruct @enforce_keys
 
   # List of coords that produce the 8 coordinates surrounding a given coord when added to it
-  @adjacent_deltas for x <- -1..1, y <- -1..1, not (x == 0 and y == 0), do: {x, y}
+  @all_adjacent_deltas for x <- -1..1, y <- -1..1, not (x == 0 and y == 0), do: {x, y}
+
+  # List of coords that produce the 4 coordinates horizontally/vertically adjacent to a given coord when added to it
+  @cardinal_adjacent_deltas for x <- -1..1, y <- -1..1, abs(x) + abs(y) == 1, do: {x, y}
+
+  # List of coords that produce the 4 coordinates diagonally adjacent to a given coord when added to it
+  @intercardinal_adjacent_deltas for x <- -1..1, y <- -1..1, abs(x) + abs(y) == 2, do: {x, y}
+
+  @adjacency_deltas_by_type %{
+    all: @all_adjacent_deltas,
+    cardinal: @cardinal_adjacent_deltas,
+    intercardinal: @intercardinal_adjacent_deltas
+  }
 
   @spec from_input(String.t()) :: t()
   def from_input(input) do
@@ -49,25 +63,90 @@ defmodule AdventOfCode.CharGrid do
     t.grid[coords]
   end
 
-  @doc "Applies `fun` to each cell in the CharGrid to produce a new CharGrid."
-  @spec map(t(), ({coordinates, char} -> char)) :: t()
+  @doc "Applies `fun` to each cell to produce a new CharGrid. `fun` must return a char."
+  @spec map(t(), (cell -> char)) :: t()
   def map(%T{} = t, fun) do
-    %{t | grid: for({coords, _} = entry <- t.grid, into: %{}, do: {coords, fun.(entry)})}
+    %{t | grid: for({coords, _} = cell <- t.grid, into: %{}, do: {coords, fun.(cell)})}
   end
 
-  @doc "Returns the number of cells in the CharGrid containing `char`."
+  @doc "Converts the grid to a list of cells."
+  @spec to_list(t()) :: list(cell)
+  def to_list(%T{} = t) do
+    Map.to_list(t.grid)
+  end
+
+  @doc "Returns the number of cells for which `predicate` returns a truthy value."
+  @spec count(t(), (cell -> as_boolean(any()))) :: non_neg_integer()
+  def count(%T{} = t, predicate) do
+    Enum.count(t.grid, predicate)
+  end
+
+  @doc "Returns the number of cells containing `char`."
   @spec count_chars(t(), char) :: non_neg_integer()
   def count_chars(%T{} = t, char) do
-    Enum.count(t.grid, fn {_, c} -> c == char end)
+    count(t, fn {_, c} -> c == char end)
   end
 
-  @doc "Returns a list of values from the up to 8 cells adjacent to the one at `coords`."
-  @spec adjacent_values(t(), coordinates) :: list(char)
-  def adjacent_values(%T{} = t, coords) do
-    @adjacent_deltas
+  @doc "Returns a list of cells for which `predicate` returns a truthy value."
+  @spec filter_cells(t(), (cell -> as_boolean(any()))) :: list(cell)
+  def filter_cells(%T{} = t, predicate) do
+    Enum.filter(t.grid, predicate)
+  end
+
+  @type adjacency_type :: :all | :cardinal | :intercardinal
+
+  @doc """
+  Returns a list of cells adjacent to the one at `coords`.
+
+  The type of adjacency is determined by the third argument:
+
+  - `:all` (default behavior):
+    ```
+    OOO
+    O*O
+    OOO
+    ```
+  - `:cardinal`:
+    ```
+    .O.
+    O*O
+    .O.
+    ```
+  - `:intercardinal`:
+    ```
+    O.O
+    .*.
+    O.O
+    ```
+  """
+  @spec adjacent_cells(t(), coordinates, adjacency_type) :: list(cell)
+  def adjacent_cells(%T{} = t, coords, adjacency_type \\ :all) do
+    @adjacency_deltas_by_type[adjacency_type]
     |> Enum.map(&sum_coordinates(coords, &1))
-    |> Enum.map(&at(t, &1))
-    |> Enum.reject(&is_nil/1)
+    |> Enum.map(fn adjacent_coords -> {adjacent_coords, at(t, adjacent_coords)} end)
+    |> Enum.reject(fn {_coords, value} -> is_nil(value) end)
+  end
+
+  @doc """
+  Convenience function that behaves the same as `adjacent_cells/3`,
+  but returns only the char value of each adjacent cell.
+  """
+  @spec adjacent_values(t(), coordinates, adjacency_type) :: list(char)
+  def adjacent_values(%T{} = t, coords, adjacency_type \\ :all) do
+    t
+    |> adjacent_cells(coords, adjacency_type)
+    |> Enum.map(&elem(&1, 1))
+  end
+
+  @doc """
+  Convenience function that behaves the same as `adjacent_cells/3`,
+  but returns only the coordinates of each adjacent cell.
+  """
+  @spec adjacent_coords(t(), coordinates, adjacency_type) :: list(coordinates)
+  def adjacent_coords(%T{} = t, coords, adjacency_type \\ :all) do
+    t
+    |> adjacent_cells(coords, adjacency_type)
+    |> Enum.map(&elem(&1, 0))
   end
 
   @doc """
@@ -78,7 +157,7 @@ defmodule AdventOfCode.CharGrid do
   """
   @spec queen_move_values(t(), coordinates, char) :: list(char)
   def queen_move_values(%T{} = t, coords, empty_char \\ ?.) do
-    @adjacent_deltas
+    @all_adjacent_deltas
     |> Enum.map(&find_nonempty_on_line(t, &1, sum_coordinates(coords, &1), empty_char))
     |> Enum.reject(&is_nil/1)
   end
