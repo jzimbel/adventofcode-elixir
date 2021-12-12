62613775e96f0e937f31adf155d4c9529e3fee03
Jon Zimbel <jzimbel@gmail.com>
2021-12-12 14:15:18 -0500

    Day 9 solution and tests

--- lib/advent_of_code/solution/year_2021/day_09.ex
+++ lib/advent_of_code/solution/year_2021/day_09.ex
@@ -1,7 +1,83 @@
 defmodule AdventOfCode.Solution.Year2021.Day09 do
-  def part1(_input) do
+  alias AdventOfCode.CharGrid
+
+  def part1(input) do
+    input
+    |> CharGrid.from_input()
+    |> local_minima_risk_levels()
+    |> Enum.sum()
   end
 
-  def part2(_input) do
+  def part2(input) do
+    input
+    |> CharGrid.from_input()
+    |> get_basins()
+    |> Enum.map(&MapSet.size/1)
+    |> Enum.sort(:desc)
+    |> Enum.take(3)
+    |> Enum.product()
+  end
+
+  defp local_minima_risk_levels(grid) do
+    grid
+    |> CharGrid.filter_cells(fn {coords, value} ->
+      grid
+      |> CharGrid.adjacent_values(coords)
+      |> Enum.all?(&(value < &1))
+    end)
+    # codepoint for '0' (aka ?0 in Elixir syntax) == 48;
+    # The problem calls for value + 1, so subtract 47 from codepoint
+    |> Enum.map(fn {_coords, char} -> char - 47 end)
+  end
+
+  defp get_basins(grid) do
+    grid
+    |> CharGrid.to_list()
+    |> non_nine_coords()
+    |> Enum.map(fn coords ->
+      adjacent_non_nines =
+        grid
+        |> CharGrid.adjacent_cells(coords, :cardinal)
+        |> non_nine_coords()
+
+      MapSet.new([coords | adjacent_non_nines])
+    end)
+    |> merge_basins()
+  end
+
+  defp non_nine_coords(cells) do
+    Enum.flat_map(cells, fn
+      {_coords, ?9} -> []
+      {coords, _value} -> [coords]
+    end)
+  end
+
+  defp merge_basins(seed_basins, merged_basins \\ [])
+
+  defp merge_basins([], merged_basins), do: merged_basins
+
+  defp merge_basins([seed_basin | seed_basins], merged_basins) do
+    {remaining, merged_basin} = flood_basin(seed_basin, seed_basins)
+
+    merge_basins(remaining, [merged_basin | merged_basins])
+  end
+
+  defp flood_basin(basin, seed_basins) do
+    import MapSet, only: [intersection: 2, size: 1, union: 2]
+
+    {remaining_seed_basins, new_basin} =
+      Enum.flat_map_reduce(seed_basins, basin, fn seed_basin, acc ->
+        if size(intersection(acc, seed_basin)) > 0 do
+          {[], union(acc, seed_basin)}
+        else
+          {[seed_basin], acc}
+        end
+      end)
+
+    if size(basin) == size(new_basin) do
+      {remaining_seed_basins, new_basin}
+    else
+      flood_basin(new_basin, remaining_seed_basins)
+    end
   end
 end
--- test/advent_of_code/solution/year_2021/day_09_test.exs
+++ test/advent_of_code/solution/year_2021/day_09_test.exs
@@ -6,21 +6,24 @@ defmodule AdventOfCode.Solution.Year2021.Day09Test do
   setup do
     [
       input: """
+      2199943210
+      3987894921
+      9856789892
+      8767896789
+      9899965678
       """
     ]
   end
 
-  @tag :skip
   test "part1", %{input: input} do
     result = part1(input)
 
-    assert result
+    assert result == 15
   end
 
-  @tag :skip
   test "part2", %{input: input} do
     result = part2(input)
 
-    assert result
+    assert result == 1134
   end
 end
