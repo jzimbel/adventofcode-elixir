20661a387d9b76fef91ab0b450827816f1460243
Jon Zimbel <jzimbel@gmail.com>
2021-12-12 20:49:42 -0500

    Day 12 solution and tests

--- lib/advent_of_code/solution/year_2021/day_12.ex
+++ lib/advent_of_code/solution/year_2021/day_12.ex
@@ -1,7 +1,69 @@
 defmodule AdventOfCode.Solution.Year2021.Day12 do
-  def part1(_input) do
+  def part1(input) do
+    input
+    |> parse_cave_map()
+    |> count_paths(&visitable_p1?/2)
   end
 
-  def part2(_input) do
+  def part2(input) do
+    input
+    |> parse_cave_map()
+    |> count_paths(&visitable_p2?/2)
+  end
+
+  defp count_paths(cave \\ "start", visit_counts \\ %{}, cave_map, visitable_fn)
+
+  defp count_paths("end", _, _, _), do: 1
+
+  defp count_paths(cave, visit_counts, cave_map, visitable_fn) do
+    visit_counts = Map.update(visit_counts, cave, 1, &(&1 + 1))
+
+    cave_map[cave]
+    |> Enum.filter(&visitable_fn.(&1, visit_counts))
+    |> Enum.map(&count_paths(&1, visit_counts, cave_map, visitable_fn))
+    |> Enum.sum()
+  end
+
+  # Part 1 visitable checker
+  defp visitable_p1?(<<first_char, _rest::binary>> = small_cave, visit_counts)
+       when first_char in ?a..?z do
+    Map.get(visit_counts, small_cave, 0) == 0
+  end
+
+  defp visitable_p1?(_cave, _visit_counts), do: true
+
+  # Part 2 visitable checker
+  defp visitable_p2?(terminus, visit_counts) when terminus in ~w[start end] do
+    Map.get(visit_counts, terminus, 0) == 0
+  end
+
+  defp visitable_p2?(<<first_char, _rest::binary>> = small_cave, visit_counts)
+       when first_char in ?a..?z do
+    case Map.get(visit_counts, small_cave, 0) do
+      0 -> true
+      1 -> no_small_caves_visited_twice?(visit_counts)
+      _ -> false
+    end
+  end
+
+  defp visitable_p2?(_cave, _visit_counts), do: true
+
+  defp no_small_caves_visited_twice?(visit_counts) do
+    visit_counts
+    |> Enum.filter(fn {cave, _} -> small_cave?(cave) end)
+    |> Enum.all?(fn {_, visit_count} -> visit_count < 2 end)
+  end
+
+  defp small_cave?(<<first_char, _rest::binary>>), do: first_char in ?a..?z
+
+  defp parse_cave_map(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&Regex.run(~r/(\w+)-(\w+)/, &1, capture: :all_but_first))
+    |> Enum.reduce(%{}, fn [cave1, cave2], cave_map ->
+      cave_map
+      |> Map.update(cave1, [cave2], fn reachable -> [cave2 | reachable] end)
+      |> Map.update(cave2, [cave1], fn reachable -> [cave1 | reachable] end)
+    end)
   end
 end
--- test/advent_of_code/solution/year_2021/day_12_test.exs
+++ test/advent_of_code/solution/year_2021/day_12_test.exs
@@ -5,22 +5,59 @@ defmodule AdventOfCode.Solution.Year2021.Day12Test do
 
   setup do
     [
-      input: """
+      input_smol: """
+      start-A
+      start-b
+      A-c
+      A-b
+      b-d
+      A-end
+      b-end
+      """,
+      input_med: """
+      dc-end
+      HN-start
+      start-kj
+      dc-start
+      dc-HN
+      LN-dc
+      HN-end
+      kj-sa
+      kj-HN
+      kj-dc
+      """,
+      input_beeg: """
+      fs-end
+      he-DX
+      fs-he
+      start-DX
+      pj-DX
+      end-zg
+      zg-sl
+      zg-pj
+      pj-he
+      RW-he
+      fs-DX
+      pj-RW
+      zg-RW
+      start-pj
+      he-WI
+      zg-he
+      pj-fs
+      start-RW
       """
     ]
   end
 
-  @tag :skip
-  test "part1", %{input: input} do
-    result = part1(input)
-
-    assert result
+  test "part1", context do
+    assert 10 == part1(context.input_smol)
+    assert 19 == part1(context.input_med)
+    assert 226 == part1(context.input_beeg)
   end
 
-  @tag :skip
-  test "part2", %{input: input} do
-    result = part2(input)
-
-    assert result
+  test "part2", context do
+    assert 36 == part2(context.input_smol)
+    assert 103 == part2(context.input_med)
+    assert 3509 == part2(context.input_beeg)
   end
 end
