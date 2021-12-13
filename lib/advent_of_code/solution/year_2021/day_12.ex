bc6b5c7bc5388154282c9ebc5b4b2a5dea61a102
Jon Zimbel <jzimbel@gmail.com>
2021-12-12 22:06:56 -0500

    Make day 12 solution faster with atoms

--- lib/advent_of_code/solution/year_2021/day_12.ex
+++ lib/advent_of_code/solution/year_2021/day_12.ex
@@ -11,12 +11,16 @@ defmodule AdventOfCode.Solution.Year2021.Day12 do
     |> count_paths(&visitable_p2?/2)
   end
 
-  defp count_paths(cave \\ "start", visit_counts \\ %{}, cave_map, visitable_fn)
+  defp count_paths(cave \\ :start, visit_counts \\ %{}, cave_map, visitable_fn)
 
-  defp count_paths("end", _, _, _), do: 1
+  defp count_paths(:end, _, _, _), do: 1
 
   defp count_paths(cave, visit_counts, cave_map, visitable_fn) do
-    visit_counts = Map.update(visit_counts, cave, 1, &(&1 + 1))
+    visit_counts =
+      case cave do
+        {:small, id} -> Map.update(visit_counts, id, 1, &(&1 + 1))
+        _ -> visit_counts
+      end
 
     cave_map[cave]
     |> Enum.filter(&visitable_fn.(&1, visit_counts))
@@ -25,45 +29,45 @@ defmodule AdventOfCode.Solution.Year2021.Day12 do
   end
 
   # Part 1 visitable checker
-  defp visitable_p1?(<<first_char, _rest::binary>> = small_cave, visit_counts)
-       when first_char in ?a..?z do
-    Map.get(visit_counts, small_cave, 0) == 0
+  defp visitable_p1?(:start, _visit_counts), do: false
+
+  defp visitable_p1?({:small, id}, visit_counts) do
+    Map.get(visit_counts, id, 0) == 0
   end
 
   defp visitable_p1?(_cave, _visit_counts), do: true
 
   # Part 2 visitable checker
-  defp visitable_p2?(terminus, visit_counts) when terminus in ~w[start end] do
-    Map.get(visit_counts, terminus, 0) == 0
-  end
+  defp visitable_p2?(:start, _visit_counts), do: false
 
-  defp visitable_p2?(<<first_char, _rest::binary>> = small_cave, visit_counts)
-       when first_char in ?a..?z do
-    case Map.get(visit_counts, small_cave, 0) do
+  defp visitable_p2?({:small, id}, visit_counts) do
+    case Map.get(visit_counts, id, 0) do
       0 -> true
-      1 -> no_small_caves_visited_twice?(visit_counts)
+      1 -> Enum.all?(visit_counts, fn {_, visit_count} -> visit_count < 2 end)
       _ -> false
     end
   end
 
   defp visitable_p2?(_cave, _visit_counts), do: true
 
-  defp no_small_caves_visited_twice?(visit_counts) do
-    visit_counts
-    |> Enum.filter(fn {cave, _} -> small_cave?(cave) end)
-    |> Enum.all?(fn {_, visit_count} -> visit_count < 2 end)
-  end
-
-  defp small_cave?(<<first_char, _rest::binary>>), do: first_char in ?a..?z
-
   defp parse_cave_map(input) do
     input
     |> String.split("\n", trim: true)
     |> Enum.map(&Regex.run(~r/(\w+)-(\w+)/, &1, capture: :all_but_first))
+    |> Enum.map(fn entries -> Enum.map(entries, &parse_cave/1) end)
     |> Enum.reduce(%{}, fn [cave1, cave2], cave_map ->
       cave_map
       |> Map.update(cave1, [cave2], fn reachable -> [cave2 | reachable] end)
       |> Map.update(cave2, [cave1], fn reachable -> [cave1 | reachable] end)
     end)
   end
+
+  defp parse_cave("start"), do: :start
+  defp parse_cave("end"), do: :end
+
+  defp parse_cave(<<first_char, _rest::binary>> = cave) when first_char in ?a..?z do
+    {:small, String.to_atom(cave)}
+  end
+
+  defp parse_cave(cave), do: {:large, String.to_atom(cave)}
 end
