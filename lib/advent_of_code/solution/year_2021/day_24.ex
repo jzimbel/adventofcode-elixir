4b506443665cface9df700e7facbdde6b62f34ca
Jon Zimbel <jzimbel@gmail.com>
2022-12-03 16:32:59 -0500

    Day 24 **incomplete** solution and tests

--- lib/advent_of_code/solution/year_2021/day_24.ex
+++ lib/advent_of_code/solution/year_2021/day_24.ex
@@ -1,7 +1,92 @@
 defmodule AdventOfCode.Solution.Year2021.Day24 do
-  def part1(_input) do
+  ### NOTE: Incomplete.
+  ### NOTE: Part 1 solution is too inefficient.
+
+  def part1(input) do
+    input
+    |> parse_program()
+    |> then(fn program ->
+      Enum.find(stream_model_numbers_desc(), &(program.(&1).z == 0))
+    end)
   end
 
   def part2(_input) do
   end
+
+  defp stream_model_numbers_desc(start \\ 99_999_999_999_999) do
+    start
+    |> Stream.iterate(&(&1 - 1))
+    |> Stream.map(&:io_lib.format("~14..0B", [&1]))
+    |> Stream.map(fn chars -> Enum.map(chars, &(&1 - ?0)) end)
+    |> Stream.reject(&Enum.member?(&1, 0))
+  end
+
+  defp get_instr({:inp, [a]}) do
+    fn {mem, [input | inputs]} -> {%{mem | a => input}, inputs} end
+  end
+
+  defp get_instr({:add, [a, b]}) when is_atom(b) do
+    fn {mem, inputs} -> {%{mem | a => Map.fetch!(mem, a) + Map.fetch!(mem, b)}, inputs} end
+  end
+
+  defp get_instr({:mul, [a, b]}) when is_atom(b) do
+    fn {mem, inputs} -> {%{mem | a => Map.fetch!(mem, a) * Map.fetch!(mem, b)}, inputs} end
+  end
+
+  defp get_instr({:div, [a, b]}) when is_atom(b) do
+    fn {mem, inputs} -> {%{mem | a => div(Map.fetch!(mem, a), Map.fetch!(mem, b))}, inputs} end
+  end
+
+  defp get_instr({:mod, [a, b]}) when is_atom(b) do
+    fn {mem, inputs} -> {%{mem | a => rem(Map.fetch!(mem, a), Map.fetch!(mem, b))}, inputs} end
+  end
+
+  defp get_instr({:eql, [a, b]}) when is_atom(b) do
+    fn {mem, inputs} ->
+      {%{mem | a => if(Map.fetch!(mem, a) == Map.fetch!(mem, b), do: 1, else: 0)}, inputs}
+    end
+  end
+
+  defp get_instr({:add, [a, b]}) do
+    fn {mem, inputs} -> {%{mem | a => Map.fetch!(mem, a) + b}, inputs} end
+  end
+
+  defp get_instr({:mul, [a, b]}) do
+    fn {mem, inputs} -> {%{mem | a => Map.fetch!(mem, a) * b}, inputs} end
+  end
+
+  defp get_instr({:div, [a, b]}) do
+    fn {mem, inputs} -> {%{mem | a => div(Map.fetch!(mem, a), b)}, inputs} end
+  end
+
+  defp get_instr({:mod, [a, b]}) do
+    fn {mem, inputs} -> {%{mem | a => rem(Map.fetch!(mem, a), b)}, inputs} end
+  end
+
+  defp get_instr({:eql, [a, b]}) do
+    fn {mem, inputs} ->
+      {%{mem | a => if(Map.fetch!(mem, a) == b, do: 1, else: 0)}, inputs}
+    end
+  end
+
+  def parse_program(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_instr/1)
+    |> Enum.reduce(&{%{w: 0, x: 0, y: 0, z: 0}, &1}, fn instr, program ->
+      instr = get_instr(instr)
+
+      &instr.(program.(&1))
+    end)
+    |> then(&fn input -> elem(&1.(input), 0) end)
+  end
+
+  defp parse_instr(instr) do
+    [op | args] = String.split(instr)
+    {String.to_existing_atom(op), Enum.map(args, &parse_arg/1)}
+  end
+
+  defp parse_arg(arg) when arg in ~w[w x y z], do: String.to_existing_atom(arg)
+
+  defp parse_arg(n), do: String.to_integer(n)
 end
--- test/advent_of_code/solution/year_2021/day_24_test.exs
+++ test/advent_of_code/solution/year_2021/day_24_test.exs
@@ -5,9 +5,49 @@ defmodule AdventOfCode.Solution.Year2021.Day24Test do
 
   setup do
     [
-      input: """
+      negate: """
+      inp x
+      mul x -1
+      """,
+      compare: """
+      inp z
+      inp x
+      mul z 3
+      eql z x
+      """,
+      bin_digits: """
+      inp w
+      add z w
+      mod z 2
+      div w 2
+      add y w
+      mod y 2
+      div w 2
+      add x w
+      mod x 2
+      div w 2
+      mod w 2
       """
     ]
+  end
+
+  test "parse_program", %{negate: negate, compare: compare, bin_digits: bin_digits} do
+    negate_prog = parse_program(negate)
+    assert %{x: -3} = negate_prog.([3])
+    assert %{x: 5} = negate_prog.([-5])
+
+    compare_prog = parse_program(compare)
+    assert %{z: 1} = compare_prog.([3, 9])
+    assert %{z: 1} = compare_prog.([-8, -24])
+    assert %{z: 0} = compare_prog.([3, 2])
+    assert %{z: 0} = compare_prog.([-8, -25])
+
+    bin_digits_prog = parse_program(bin_digits)
+    assert %{w: 0, x: 0, y: 0, z: 1} = bin_digits_prog.([1])
+    assert %{w: 1, x: 0, y: 1, z: 0} = bin_digits_prog.([10])
+    assert %{w: 1, x: 0, y: 1, z: 0} = bin_digits_prog.([26])
+    assert %{w: 1, x: 1, y: 1, z: 1} = bin_digits_prog.([31])
+    assert %{w: 0, x: 0, y: 0, z: 0} = bin_digits_prog.([32])
   end
 
   @tag :skip
