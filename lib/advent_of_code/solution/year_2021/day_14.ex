9b544b0b2cc982fe4be6073b4b8ed409a7a644ca
Jon Zimbel <jzimbel@gmail.com>
2022-12-03 15:33:00 -0500

    Day 14 solution and tests

--- lib/advent_of_code/solution/year_2021/day_14.ex
+++ lib/advent_of_code/solution/year_2021/day_14.ex
@@ -1,7 +1,88 @@
 defmodule AdventOfCode.Solution.Year2021.Day14 do
-  def part1(_input) do
+  def part1(input) do
+    measure_frequencies_at(input, 10)
   end
 
-  def part2(_input) do
+  def part2(input) do
+    measure_frequencies_at(input, 40)
+  end
+
+  defp measure_frequencies_at(input, step) do
+    {frequencies, rules, first_char, last_char} = parse_input(input)
+
+    frequencies
+    |> Stream.iterate(&insert(&1, rules))
+    |> Enum.at(step)
+    |> bigram_frequencies_to_unigram_frequencies(first_char, last_char)
+    |> Map.values()
+    |> Enum.min_max()
+    |> then(fn {least_common_count, most_common_count} ->
+      most_common_count - least_common_count
+    end)
+  end
+
+  defp insert(freqs, rules) do
+    update_keylist_and_merge_values(
+      freqs,
+      fn {[char_a, char_b] = chars, count} ->
+        insertion = rules[chars]
+        [{[char_a, insertion], count}, {[insertion, char_b], count}]
+      end,
+      &Enum.sum/1
+    )
+  end
+
+  defp bigram_frequencies_to_unigram_frequencies(freqs, char, char) do
+    do_bigram_frequencies_to_unigram_frequencies(freqs, fn
+      {^char, doubled_freq} -> {char, ceil(doubled_freq / 2) + 1}
+      {other_char, doubled_freq} -> {other_char, ceil(doubled_freq / 2)}
+    end)
+  end
+
+  defp bigram_frequencies_to_unigram_frequencies(freqs, _, _) do
+    do_bigram_frequencies_to_unigram_frequencies(freqs, fn
+      {char, doubled_freq} -> {char, ceil(doubled_freq / 2)}
+    end)
+  end
+
+  defp do_bigram_frequencies_to_unigram_frequencies(freqs, un_doubler) do
+    freqs
+    |> update_keylist_and_merge_values(
+      fn {[char_a, char_b], freq} -> [{char_a, freq}, {char_b, freq}] end,
+      &Enum.sum/1
+    )
+    |> Enum.into(%{}, un_doubler)
+  end
+
+  defp update_keylist_and_merge_values(keylist, updater, merger) do
+    keylist
+    |> Enum.flat_map(updater)
+    |> Enum.group_by(&elem(&1, 0), &elem(&1, 1))
+    |> Enum.map(fn {k, values} -> {k, merger.(values)} end)
+  end
+
+  defp parse_input(input) do
+    [first_char | rest_chars] = String.to_charlist(input)
+    last_char = List.last(rest_chars)
+
+    input
+    |> String.split("\n\n", trim: true)
+    |> then(fn [polymer, rules] ->
+      {bigram_frequencies(polymer), parse_rules(rules), first_char, last_char}
+    end)
+  end
+
+  defp bigram_frequencies(string) do
+    string
+    |> String.to_charlist()
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.frequencies()
+  end
+
+  defp parse_rules(rules) do
+    rules
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn <<char_a, char_b, " -> ", insertion>> -> {[char_a, char_b], insertion} end)
+    |> Enum.into(%{})
   end
 end
--- test/advent_of_code/solution/year_2021/day_14_test.exs
+++ test/advent_of_code/solution/year_2021/day_14_test.exs
@@ -6,21 +6,37 @@ defmodule AdventOfCode.Solution.Year2021.Day14Test do
   setup do
     [
       input: """
+      NNCB
+
+      CH -> B
+      HH -> N
+      CB -> H
+      NH -> C
+      HB -> C
+      HC -> B
+      HN -> C
+      NN -> C
+      BH -> H
+      NC -> B
+      NB -> B
+      BN -> B
+      BB -> N
+      BC -> B
+      CC -> N
+      CN -> C
       """
     ]
   end
 
-  @tag :skip
   test "part1", %{input: input} do
     result = part1(input)
 
-    assert result
+    assert result == 1588
   end
 
-  @tag :skip
   test "part2", %{input: input} do
     result = part2(input)
 
-    assert result
+    assert result == 2_188_189_693_529
   end
 end
