c5c7b7056a868845931745349f5ec548cf604f86
Jon Zimbel <jzimbel@gmail.com>
2022-12-03 15:37:44 -0500

    Day 17 solution and tests

--- lib/advent_of_code/solution/year_2021/day_17.ex
+++ lib/advent_of_code/solution/year_2021/day_17.ex
@@ -1,7 +1,63 @@
 defmodule AdventOfCode.Solution.Year2021.Day17 do
-  def part1(_input) do
+  def part1(input) do
+    {_x_target, ymin..ymax} = target = parse_input(input)
+
+    -ymin..-ymax//-1
+    |> Stream.map(&simulate_shot({0, &1}, target))
+    |> Enum.find(&first_step_hit?(&1, target))
+    |> Stream.map(fn {_x, y} -> y end)
+    |> Enum.max()
   end
 
-  def part2(_input) do
+  def part2(input) do
+    {_xmin..xmax, ymin.._ymax} = target = parse_input(input)
+
+    for(vx <- 0..xmax, vy <- ymin..-ymin, do: {vx, vy})
+    |> Stream.map(&simulate_shot(&1, target))
+    |> Enum.count(&hits_target?(&1, target))
   end
+
+  defp simulate_shot(v, target) do
+    {v, {0, 0}}
+    |> Stream.iterate(&step/1)
+    |> Stream.map(&elem(&1, 1))
+    |> Stream.take_while(&in_simulation_bounds?(&1, target))
+  end
+
+  defp step({{vx, vy}, {x, y}}) do
+    {{vx - unit(vx), vy - 1}, {x + vx, y + vy}}
+  end
+
+  defp in_simulation_bounds?({x, y}, {xmin..xmax, ymin.._ymax}) do
+    x in min(0, xmin)..max(0, xmax) and y >= ymin
+  end
+
+  # Detects shots where the probe hits the target's y bounds on its first step below y=0
+  defp first_step_hit?(shot, {_x_target, y_target}) do
+    shot
+    |> Stream.chunk_every(2, 1, :discard)
+    |> Enum.any?(fn [{_x1, y1}, {_x2, y2}] -> y1 >= 0 and y2 in y_target end)
+  end
+
+  defp hits_target?(shot, target) do
+    Enum.any?(shot, &on_target?(&1, target))
+  end
+
+  defp on_target?({x, y}, {x_target, y_target}) do
+    x in x_target and y in y_target
+  end
+
+  defp parse_input(input) do
+    ~r/(-?\d+)\.\.(-?\d+)/
+    |> Regex.scan(input, capture: :all_but_first)
+    |> List.flatten()
+    |> Enum.map(&String.to_integer/1)
+    |> then(fn [xmin, xmax, ymin, ymax] ->
+      {xmin..xmax, ymin..ymax}
+    end)
+  end
+
+  defp unit(0), do: 0
+  defp unit(n) when n > 0, do: 1
+  defp unit(_n), do: -1
 end
--- test/advent_of_code/solution/year_2021/day_17_test.exs
+++ test/advent_of_code/solution/year_2021/day_17_test.exs
@@ -6,21 +6,20 @@ defmodule AdventOfCode.Solution.Year2021.Day17Test do
   setup do
     [
       input: """
+      target area: x=20..30, y=-10..-5
       """
     ]
   end
 
-  @tag :skip
   test "part1", %{input: input} do
     result = part1(input)
 
-    assert result
+    assert result == 45
   end
 
-  @tag :skip
   test "part2", %{input: input} do
     result = part2(input)
 
-    assert result
+    assert result == 112
   end
 end
