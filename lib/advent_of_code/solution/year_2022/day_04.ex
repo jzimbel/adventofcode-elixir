14c7ba28bc2f0702a66a38b996b2d525d3331856
Jon Zimbel <jzimbel@gmail.com>
2022-12-04 09:40:30 -0500

    Day 4 solution and tests

--- lib/advent_of_code/solution/year_2022/day_04.ex
+++ lib/advent_of_code/solution/year_2022/day_04.ex
@@ -1,7 +1,29 @@
 defmodule AdventOfCode.Solution.Year2022.Day04 do
-  def part1(_input) do
+  def part1(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_ints/1)
+    |> Enum.count(&superset_subset?/1)
   end
 
-  def part2(_input) do
+  def part2(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_ints/1)
+    |> Enum.count(&overlap?/1)
+  end
+
+  defp parse_ints(line) do
+    ~r/^(\d+)-(\d+),(\d+)-(\d+)$/
+    |> Regex.run(line, capture: :all_but_first)
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  defp superset_subset?([a, b, x, y]) do
+    (a <= x and b >= y) or (a >= x and b <= y)
+  end
+
+  defp overlap?([a, b, x, y]) do
+    not Range.disjoint?(a..b, x..y)
   end
 end
--- test/advent_of_code/solution/year_2022/day_04_test.exs
+++ test/advent_of_code/solution/year_2022/day_04_test.exs
@@ -6,21 +6,26 @@ defmodule AdventOfCode.Solution.Year2022.Day04Test do
   setup do
     [
       input: """
+      2-4,6-8
+      2-3,4-5
+      5-7,7-9
+      2-8,3-7
+      6-6,4-6
+      2-6,4-8
       """
     ]
   end
 
-  @tag :skip
   test "part1", %{input: input} do
     result = part1(input)
 
-    assert result
+    assert result == 2
   end
 
   @tag :skip
   test "part2", %{input: input} do
     result = part2(input)
 
-    assert result
+    assert result == 4
   end
 end
