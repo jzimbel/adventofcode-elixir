405dec40adacbaed9ed1e6618f1b78719ceb479b
Jon Zimbel <jzimbel@gmail.com>
2022-12-08 20:24:29 -0500

    Clean up day 8 solution

--- lib/advent_of_code/solution/year_2022/day_08.ex
+++ lib/advent_of_code/solution/year_2022/day_08.ex
@@ -2,42 +2,46 @@ defmodule AdventOfCode.Solution.Year2022.Day08 do
   alias AdventOfCode.CharGrid
 
   def part1(input) do
-    grid = CharGrid.from_input(input)
+    {grid, sight_lines_by_tree} = parse(input)
 
-    interior_visible_count =
-      for(
-        x <- 1..(grid.width - 2)//1,
-        y <- 1..(grid.height - 2)//1,
-        do: {x, y}
-      )
-      |> Enum.map(&{CharGrid.at(grid, &1), CharGrid.lines_of_values(grid, &1, :cardinal)})
-      |> Enum.count(fn {height, sight_lines} ->
-        Enum.any?(sight_lines, fn heights ->
-          Enum.all?(heights, &(&1 < height))
-        end)
-      end)
-
+    interior_visible_count = Enum.count(sight_lines_by_tree, &visible?/1)
     perimeter_tree_count = 2 * grid.width + 2 * (grid.height - 2)
 
     interior_visible_count + perimeter_tree_count
   end
 
   def part2(input) do
-    grid = CharGrid.from_input(input)
+    {_grid, sight_lines_by_tree} = parse(input)
 
-    for(
-      x <- 1..(grid.width - 2)//1,
-      y <- 1..(grid.height - 2)//1,
-      do: {x, y}
-    )
-    |> Enum.map(&{CharGrid.at(grid, &1), CharGrid.lines_of_values(grid, &1, :cardinal)})
+    sight_lines_by_tree
     |> Enum.map(&scenic_score/1)
     |> Enum.max()
   end
 
-  defp scenic_score({height, sight_lines}) do
+  defp parse(input) do
+    grid = CharGrid.from_input(input)
+
+    sight_lines_by_tree =
+      for(
+        # Trees on the perimeter are excluded
+        x <- 1..(grid.width - 2)//1,
+        y <- 1..(grid.height - 2)//1,
+        do: {x, y}
+      )
+      |> Enum.map(&{CharGrid.at(grid, &1), CharGrid.lines_of_values(grid, &1, :cardinal)})
+
+    {grid, sight_lines_by_tree}
+  end
+
+  defp visible?({tree, sight_lines}) do
+    Enum.any?(sight_lines, fn other_trees ->
+      Enum.all?(other_trees, &(&1 < tree))
+    end)
+  end
+
+  defp scenic_score({tree, sight_lines}) do
     sight_lines
-    |> Enum.map(&count_visible(&1, height))
+    |> Enum.map(&count_visible(&1, tree))
     |> Enum.product()
   end
 
@@ -45,6 +49,6 @@ defmodule AdventOfCode.Solution.Year2022.Day08 do
   # All of the relevant `Enum` functions omit the first element that fails the condition,
   # so I needed to define my own recursive function.
   defp count_visible([], _), do: 0
-  defp count_visible([tree | _trees], height) when tree >= height, do: 1
-  defp count_visible([_tree | trees], height), do: 1 + count_visible(trees, height)
+  defp count_visible([tree | _trees], from_tree) when tree >= from_tree, do: 1
+  defp count_visible([_tree | trees], from_tree), do: 1 + count_visible(trees, from_tree)
 end
