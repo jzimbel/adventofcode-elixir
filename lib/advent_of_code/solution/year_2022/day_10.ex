a70968ec10517b37e75704a4c4876b7cfce20b76
Jon Zimbel <jzimbel@gmail.com>
2022-12-15 14:31:35 -0500

    Day 10 solution and tests

--- lib/advent_of_code/solution/year_2022/day_10.ex
+++ lib/advent_of_code/solution/year_2022/day_10.ex
@@ -1,7 +1,29 @@
 defmodule AdventOfCode.Solution.Year2022.Day10 do
-  def part1(_input) do
+  def part1(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Stream.transform(1, &parse_cycles/2)
+    |> Stream.drop(19)
+    |> Stream.take_every(40)
+    |> Stream.take(6)
+    |> Enum.with_index(fn v, i -> (20 + 40 * i) * v end)
+    |> Enum.sum()
   end
 
-  def part2(_input) do
+  def part2(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Stream.transform(1, &parse_cycles/2)
+    |> Stream.zip(Stream.cycle(0..39))
+    |> Stream.map(fn
+      {signal, draw} when abs(draw - signal) <= 1 -> ?▓
+      _ -> ?░
+    end)
+    |> Stream.chunk_every(40)
+    |> Enum.intersperse(?\n)
+    |> to_string()
   end
+
+  defp parse_cycles("noop", v), do: {[v], v}
+  defp parse_cycles(<<"addx ", n::binary>>, v), do: {[v, v], v + String.to_integer(n)}
 end
