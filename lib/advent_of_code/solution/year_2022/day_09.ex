f17c2a706cba150f8b2d78cb708097f4234b2ea7
Jon Zimbel <jzimbel@gmail.com>
2022-12-15 14:24:43 -0500

    Day 9 solution and tests

--- lib/advent_of_code/solution/year_2022/day_09.ex
+++ lib/advent_of_code/solution/year_2022/day_09.ex
@@ -1,7 +1,41 @@
 defmodule AdventOfCode.Solution.Year2022.Day09 do
-  def part1(_input) do
+  defstruct [:knots, visited: MapSet.new([{0, 0}])]
+
+  def part1(input), do: solve(input, 2)
+  def part2(input), do: solve(input, 10)
+
+  defp solve(input, knot_count) do
+    initial_state = %__MODULE__{knots: List.duplicate({0, 0}, knot_count)}
+
+    final_state =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.reduce(initial_state, &process_command/2)
+
+    MapSet.size(final_state.visited)
   end
 
-  def part2(_input) do
+  for {dir, delta} <- [{?U, {0, -1}}, {?R, {1, 0}}, {?D, {0, 1}}, {?L, {-1, 0}}] do
+    defp process_command(<<unquote(dir), " ", steps::binary>>, state) do
+      for _ <- 1..String.to_integer(steps), reduce: state, do: (acc -> move(acc, unquote(delta)))
+    end
   end
+
+  defp move(%__MODULE__{knots: [h | t], visited: visited}, delta) do
+    knots = Enum.scan([add(h, delta) | t], &propagate/2)
+    visited = MapSet.put(visited, List.last(knots))
+
+    %__MODULE__{knots: knots, visited: visited}
+  end
+
+  defp propagate(t, h), do: if(adjacent?(h, t), do: t, else: move_toward(t, h))
+
+  defp adjacent?({x, y}, {x2, y2}), do: abs(x2 - x) <= 1 and abs(y2 - y) <= 1
+
+  defp move_toward({tx, ty}, {hx, hy}), do: add({tx, ty}, {get_move(hx - tx), get_move(hy - ty)})
+
+  defp get_move(0), do: 0
+  defp get_move(diff), do: div(diff, abs(diff))
+
+  defp add({x, y}, {x2, y2}), do: {x + x2, y + y2}
 end
--- test/advent_of_code/solution/year_2022/day_09_test.exs
+++ test/advent_of_code/solution/year_2022/day_09_test.exs
@@ -6,21 +6,39 @@ defmodule AdventOfCode.Solution.Year2022.Day09Test do
   setup do
     [
       input: """
+      R 4
+      U 4
+      L 3
+      D 1
+      R 4
+      D 1
+      L 5
+      R 2
+      """,
+      input2: """
+      R 5
+      U 8
+      L 8
+      D 3
+      R 17
+      D 10
+      L 25
+      U 20
       """
     ]
   end
 
-  @tag :skip
   test "part1", %{input: input} do
     result = part1(input)
 
-    assert result
+    assert result == 13
   end
 
-  @tag :skip
-  test "part2", %{input: input} do
-    result = part2(input)
+  test "part2", %{input: input, input2: input2} do
+    result1 = part2(input)
+    result2 = part2(input2)
 
-    assert result
+    assert result1 == 1
+    assert result2 == 36
   end
 end
