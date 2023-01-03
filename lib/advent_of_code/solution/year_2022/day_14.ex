c34a3de49964ae0c330a80bd372578b0a2dd848c
Jon Zimbel <jzimbel@gmail.com>
2023-01-03 10:39:39 -0500

    Clean up day 14 solution

--- lib/advent_of_code/solution/year_2022/day_14.ex
+++ lib/advent_of_code/solution/year_2022/day_14.ex
@@ -2,32 +2,36 @@ defmodule AdventOfCode.Solution.Year2022.Day14 do
   @source {500, 0}
 
   def part1(input) do
+    discard_sand = fn coords, _sand -> coords end
+
     input
-    |> stream_states(fn coords, _sand -> coords end)
+    |> stream_states(discard_sand)
     |> Stream.map(&MapSet.size/1)
     |> Stream.chunk_every(2, 1)
     |> Enum.find_index(&match?([count, count], &1))
   end
 
   def part2(input) do
+    place_sand_on_floor = &MapSet.put(&1, &2)
+
     input
-    |> stream_states(&MapSet.put(&1, &2))
+    |> stream_states(place_sand_on_floor)
     |> Enum.find_index(&MapSet.member?(&1, @source))
   end
 
-  defp stream_states(input, use_floor) do
+  defp stream_states(input, floor_fn) do
     {coords, floor_y} = parse(input)
-    Stream.iterate(coords, &drop_sand(&1, {floor_y, use_floor}))
+    Stream.iterate(coords, &drop_sand(&1, {floor_y, floor_fn}))
   end
 
   defp drop_sand(coords, floor, sand \\ @source, move \\ :d)
 
-  defp drop_sand(coords, {floor_y, use_floor} = floor, {x, y} = sand, :d) do
+  defp drop_sand(coords, {floor_y, floor_fn} = floor, {x, y} = sand, :d) do
     next = {x, y + 1}
 
     cond do
       MapSet.member?(coords, next) -> drop_sand(coords, floor, sand, :l)
-      y == floor_y - 1 -> use_floor.(coords, sand)
+      y == floor_y -> floor_fn.(coords, sand)
       true -> drop_sand(coords, floor, next, :d)
     end
   end
@@ -59,7 +63,7 @@ defmodule AdventOfCode.Solution.Year2022.Day14 do
       coords
       |> Enum.max_by(&elem(&1, 1))
       |> elem(1)
-      |> Kernel.+(2)
+      |> Kernel.+(1)
 
     {coords, floor_y}
   end
