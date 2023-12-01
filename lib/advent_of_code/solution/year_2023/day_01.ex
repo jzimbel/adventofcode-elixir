c5e7614faf8aa75167d140e825230259ddeb3cb2
Jon Zimbel <jzimbel@gmail.com>
2023-12-01 10:23:57 -0500

    Shorter and (in some cases) faster day 1 solution

--- lib/advent_of_code/solution/year_2023/day_01.ex
+++ lib/advent_of_code/solution/year_2023/day_01.ex
@@ -1,48 +1,42 @@
 defmodule AdventOfCode.Solution.Year2023.Day01 do
-  def part1(input), do: solve(input, &digits_part1/1)
-  def part2(input), do: solve(input, &digits_part2/1)
+  def part1(input), do: solve(input, &find_digit_part1/1)
+  def part2(input), do: solve(input, &find_digit_part2/1)
 
-  defp solve(input, get_digits_fn) do
+  defp solve(input, finder_fn) do
     input
     |> String.split("\n", trim: true)
-    |> Enum.map(&(&1 |> get_digits_fn.() |> Integer.undigits()))
+    |> Enum.map(fn line ->
+      [find_first_digit(line, finder_fn), find_last_digit(line, finder_fn)]
+      |> Integer.undigits()
+    end)
     |> Enum.sum()
   end
 
   defguardp is_digit(char) when char in ?0..?9
 
-  defp digits_part1(line, acc \\ [])
-  defp digits_part1("", acc), do: [List.first(acc), List.last(acc)]
-
-  defp digits_part1(<<char, rest::binary>>, acc) when is_digit(char) do
-    digits_part1(rest, put_digit(acc, char - ?0))
+  defp find_first_digit(<<_, rest::binary>> = str, finder_fn) do
+    case finder_fn.(str) do
+      {:ok, d} -> d
+      :error -> find_first_digit(rest, finder_fn)
+    end
   end
 
-  defp digits_part1(<<_, rest::binary>>, acc) do
-    digits_part1(rest, acc)
+  defp find_last_digit(line, finder_fn), do: find_last_digit(line, finder_fn, byte_size(line) - 1)
+
+  defp find_last_digit(line, finder_fn, drop_from_front) do
+    <<_::binary-size(drop_from_front), str::binary>> = line
+
+    case finder_fn.(str) do
+      {:ok, d} -> d
+      :error -> find_last_digit(line, finder_fn, drop_from_front - 1)
+    end
   end
 
-  ###
+  defp find_digit_part1(<<char, _::binary>>) when is_digit(char), do: {:ok, char - ?0}
+  defp find_digit_part1(_), do: :error
 
-  defp digits_part2(line, acc \\ [])
-  defp digits_part2("", acc), do: [List.first(acc), List.last(acc)]
-
-  defp digits_part2(<<char, rest::binary>>, acc) when is_digit(char) do
-    digits_part2(rest, put_digit(acc, char - ?0))
-  end
-
-  defp digits_part2(<<_, rest::binary>> = line, acc) do
-    acc =
-      case find_leading_word_digit(line) do
-        {:ok, digit} -> put_digit(acc, digit)
-        :error -> acc
-      end
-
-    digits_part2(rest, acc)
-  end
-
-  defp put_digit([], first_digit), do: [first_digit]
-  defp put_digit([first_digit | _], digit), do: [first_digit, digit]
+  defp find_digit_part2(<<char, _::binary>>) when is_digit(char), do: {:ok, char - ?0}
+  defp find_digit_part2(str), do: find_leading_word_digit(str)
 
   for {str, n} <- Enum.zip(~w[one two three four five six seven eight nine], 1..9) do
     defp find_leading_word_digit(unquote(str) <> _), do: {:ok, unquote(n)}
