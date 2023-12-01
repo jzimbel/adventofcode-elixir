ceedd9bbbf8ea48c33cc8ab96699fdb8b05b718c
Jon Zimbel <jzimbel@gmail.com>
2023-12-01 08:29:19 -0500

    Polish day 1 solution

--- lib/advent_of_code/solution/year_2023/day_01.ex
+++ lib/advent_of_code/solution/year_2023/day_01.ex
@@ -6,23 +6,16 @@ defmodule AdventOfCode.Solution.Year2023.Day01 do
   defp solve(input, get_digits_fn) do
     input
     |> String.split("\n", trim: true)
-    |> Enum.map(fn line ->
-      line
-      |> get_digits_fn.()
-      |> then(fn digits ->
-        [List.first(digits), List.last(digits)]
-        |> Integer.undigits()
-      end)
-    end)
+    |> Enum.map(&(&1 |> get_digits_fn.() |> Integer.undigits()))
     |> Enum.sum()
   end
 
   defp digits_part1(line, acc \\ [])
 
-  defp digits_part1("", acc), do: Enum.reverse(acc)
+  defp digits_part1("", acc), do: [List.first(acc), List.last(acc)]
 
   defp digits_part1(<<n, rest::binary>>, acc) when n in ?0..?9 do
-    digits_part1(rest, [n - ?0 | acc])
+    digits_part1(rest, put_digit(acc, n - ?0))
   end
 
   defp digits_part1(<<_, rest::binary>>, acc) do
@@ -33,35 +26,26 @@ defmodule AdventOfCode.Solution.Year2023.Day01 do
 
   defp digits_part2(line, acc \\ [])
 
-  defp digits_part2("", acc), do: Enum.reverse(acc)
+  defp digits_part2("", acc), do: [List.first(acc), List.last(acc)]
 
   defp digits_part2(<<n, rest::binary>>, acc) when n in ?0..?9 do
-    digits_part2(rest, [n - ?0 | acc])
+    digits_part2(rest, put_digit(acc, n - ?0))
   end
 
   defp digits_part2(<<_, rest::binary>> = line, acc) do
     acc =
       case lookup(line) do
-        {:ok, digit} -> [digit | acc]
+        {:ok, digit} -> put_digit(acc, digit)
         :error -> acc
       end
 
     digits_part2(rest, acc)
   end
 
-  spelled_out = %{
-    "one" => 1,
-    "two" => 2,
-    "three" => 3,
-    "four" => 4,
-    "five" => 5,
-    "six" => 6,
-    "seven" => 7,
-    "eight" => 8,
-    "nine" => 9
-  }
+  defp put_digit([], first_digit), do: [first_digit]
+  defp put_digit([first_digit | _], digit), do: [first_digit, digit]
 
-  for {str, n} <- spelled_out do
+  for {str, n} <- Enum.zip(~w[one two three four five six seven eight nine], 1..9) do
     defp lookup(unquote(str) <> _), do: {:ok, unquote(n)}
   end
 
