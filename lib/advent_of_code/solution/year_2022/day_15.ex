b42e8a9852d7a7337516e0078044179b4e7303bd
Jon Zimbel <jzimbel@gmail.com>
2023-11-30 23:29:22 -0500

    Day 15 partial solution

--- lib/advent_of_code/solution/year_2022/day_15.ex
+++ lib/advent_of_code/solution/year_2022/day_15.ex
@@ -1,7 +1,43 @@
 defmodule AdventOfCode.Solution.Year2022.Day15 do
-  def part1(_input) do
+  def part1(input, check_y \\ 2_000_000) do
+    guys = parse(input)
+
+    exceptions = MapSet.new(for(%{b: {x, ^check_y}} <- guys, do: x))
+
+    in_line =
+      guys
+      |> Enum.flat_map(&get_crossings(&1, check_y))
+      |> MapSet.new()
+
+    MapSet.size(MapSet.difference(in_line, exceptions))
   end
 
-  def part2(_input) do
+  def part2(input, search \\ 4_000_000) do
   end
+
+  defp parse(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_relation/1)
+  end
+
+  defp get_crossings(%{s: {x, y}, d: d}, check_y) do
+    then(d - abs(y - check_y), &((x - &1)..(x + &1)//1))
+  end
+
+  defp parse_relation(line) do
+    [sx, sy, bx, by] =
+      ~r/-?\d+/
+      |> Regex.scan(line)
+      |> List.flatten()
+      |> Enum.map(&String.to_integer/1)
+
+    %{s: {sx, sy}, b: {bx, by}, d: dist({sx, sy}, {bx, by})}
+  end
+
+  defp dist({x, y}, {x2, y2}) do
+    abs(x2 - x) + abs(y2 - y)
+  end
+
+  defp tuning_frequency({x, y}), do: x * 4_000_000 + y
 end
--- test/advent_of_code/solution/year_2022/day_15_test.exs
+++ test/advent_of_code/solution/year_2022/day_15_test.exs
@@ -6,21 +6,34 @@ defmodule AdventOfCode.Solution.Year2022.Day15Test do
   setup do
     [
       input: """
+      Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+      Sensor at x=9, y=16: closest beacon is at x=10, y=16
+      Sensor at x=13, y=2: closest beacon is at x=15, y=3
+      Sensor at x=12, y=14: closest beacon is at x=10, y=16
+      Sensor at x=10, y=20: closest beacon is at x=10, y=16
+      Sensor at x=14, y=17: closest beacon is at x=10, y=16
+      Sensor at x=8, y=7: closest beacon is at x=2, y=10
+      Sensor at x=2, y=0: closest beacon is at x=2, y=10
+      Sensor at x=0, y=11: closest beacon is at x=2, y=10
+      Sensor at x=20, y=14: closest beacon is at x=25, y=17
+      Sensor at x=17, y=20: closest beacon is at x=21, y=22
+      Sensor at x=16, y=7: closest beacon is at x=15, y=3
+      Sensor at x=14, y=3: closest beacon is at x=15, y=3
+      Sensor at x=20, y=1: closest beacon is at x=15, y=3
       """
     ]
   end
 
-  @tag :skip
   test "part1", %{input: input} do
-    result = part1(input)
+    result = part1(input, 10)
 
-    assert result
+    assert result == 26
   end
 
   @tag :skip
   test "part2", %{input: input} do
-    result = part2(input)
+    result = part2(input, 20)
 
-    assert result
+    assert result == 56_000_011
   end
 end
