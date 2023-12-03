0b4763fa51247de9ca308d0a58246dda60f25190
Jon Zimbel <jzimbel@gmail.com>
2023-12-02 21:52:08 -0500

    Use a struct for day 2 solution

--- lib/advent_of_code/solution/year_2023/day_02.ex
+++ lib/advent_of_code/solution/year_2023/day_02.ex
@@ -1,41 +1,59 @@
+defmodule AdventOfCode.Solution.Year2023.Day02.Play do
+  @type t :: %__MODULE__{red: non_neg_integer, green: non_neg_integer, blue: non_neg_integer}
+  defstruct red: 0, green: 0, blue: 0
+
+  def compare(p1, p2) do
+    cond do
+      p1 == p2 -> :eq
+      p1.red > p2.red or p1.green > p2.green or p1.blue > p2.blue -> :gt
+      true -> :lt
+    end
+  end
+
+  def merge(p1, p2) do
+    %__MODULE__{
+      red: max(p1.red, p2.red),
+      green: max(p1.green, p2.green),
+      blue: max(p1.blue, p2.blue)
+    }
+  end
+end
+
 defmodule AdventOfCode.Solution.Year2023.Day02 do
+  alias __MODULE__.Play
+
   def part1(input) do
     input
     |> String.split("\n", trim: true)
-    |> Enum.map(&parse_game/1)
-    |> Enum.filter(&valid_for_part1?/1)
-    |> Enum.map(fn {game_id, _plays} -> game_id end)
+    |> Stream.map(&parse_game/1)
+    |> Stream.filter(&valid_for_part1?/1)
+    |> Stream.map(fn {game_id, _plays} -> game_id end)
     |> Enum.sum()
   end
 
   def part2(input) do
     input
     |> String.split("\n", trim: true)
-    |> Enum.map(&parse_game/1)
-    |> Enum.map(&min_cube_power/1)
+    |> Stream.map(&parse_game/1)
+    |> Stream.map(&min_cube_power/1)
     |> Enum.sum()
   end
 
-  @bag %{red: 12, green: 13, blue: 14}
-  @default_play %{red: 0, green: 0, blue: 0}
+  @bag %Play{red: 12, green: 13, blue: 14}
 
   defp valid_for_part1?({_game_id, plays}) do
-    Enum.all?(plays, &(&1.red <= @bag.red and &1.green <= @bag.green and &1.blue <= @bag.blue))
+    Enum.all?(plays, &(Play.compare(&1, @bag) in [:lt, :eq]))
   end
 
   defp min_cube_power({_game_id, plays}) do
     plays
-    |> Enum.reduce(@default_play, fn play, acc ->
-      Map.merge(acc, play, fn _k, acc_val, play_val -> max(acc_val, play_val) end)
-    end)
+    |> Enum.reduce(&Play.merge/2)
+    |> Map.from_struct()
     |> Map.values()
     |> Enum.product()
   end
 
-  @type parsed_game :: {pos_integer, list(play)}
-  @type play :: %{red: non_neg_integer, green: non_neg_integer, blue: non_neg_integer}
-
-  @spec parse_game(String.t()) :: parsed_game
+  @spec parse_game(String.t()) :: {pos_integer, list(Play.t())}
   defp parse_game(line) do
     ["Game " <> game_id, plays] = String.split(line, ": ")
 
@@ -50,13 +68,11 @@ defmodule AdventOfCode.Solution.Year2023.Day02 do
   end
 
   defp parse_play(play) do
-    counts =
-      ~r/(\d+) (\w+)/
-      |> Regex.scan(play, capture: :all_but_first)
-      |> Map.new(fn [count, color] ->
-        {String.to_existing_atom(color), String.to_integer(count)}
-      end)
-
-    Map.merge(@default_play, counts)
+    ~r/(\d+) (\w+)/
+    |> Regex.scan(play, capture: :all_but_first)
+    |> Map.new(fn [count, color] ->
+      {String.to_existing_atom(color), String.to_integer(count)}
+    end)
+    |> then(&struct(Play, &1))
   end
 end
