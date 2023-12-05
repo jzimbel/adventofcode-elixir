bbaa4574ec05bde16f68a529b8a4632e78ba9308
Jon Zimbel <jzimbel@gmail.com>
2023-12-05 12:36:17 -0500

    Day 4 solution and tests

--- lib/advent_of_code/solution/year_2023/day_04.ex
+++ lib/advent_of_code/solution/year_2023/day_04.ex
@@ -1,7 +1,51 @@
 defmodule AdventOfCode.Solution.Year2023.Day04 do
-  def part1(_input) do
+  def part1(input) do
+    input
+    |> parse()
+    |> Enum.map(&score_card/1)
+    |> Enum.sum()
   end
 
-  def part2(_input) do
+  def part2(input) do
+    input
+    |> parse()
+    |> count_cards()
+  end
+
+  def parse(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_card/1)
+  end
+
+  defp score_card({_, 0}), do: 0
+  defp score_card({_, n}), do: Integer.pow(2, n - 1)
+
+  defp count_cards(cards, acc \\ 0)
+  defp count_cards([], acc), do: acc
+
+  defp count_cards([{self_copy_count, 0} | children], acc) do
+    count_cards(children, acc + self_copy_count)
+  end
+
+  defp count_cards([{self_copy_count, child_copy_count} | children], acc) do
+    {children_to_copy, unchanged} = Enum.split(children, child_copy_count)
+
+    new_children =
+      for {sub_self_copy_count, sub_child_copy_count} <- children_to_copy do
+        {sub_self_copy_count + self_copy_count, sub_child_copy_count}
+      end
+
+    count_cards(new_children ++ unchanged, acc + self_copy_count)
+  end
+
+  defp parse_card(line) do
+    [_card_number, contents] = String.split(line, ": ")
+
+    [winners_str, hand_str] = String.split(contents, " | ")
+    winners = MapSet.new(String.split(winners_str))
+    hand = MapSet.new(String.split(hand_str))
+
+    {1, MapSet.size(MapSet.intersection(winners, hand))}
   end
 end
