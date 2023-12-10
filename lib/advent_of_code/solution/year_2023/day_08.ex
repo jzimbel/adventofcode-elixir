ea6bbf3d0228c70b3d8471eae1192fda9d94f27f
Jon Zimbel <jzimbel@gmail.com>
2023-12-10 10:26:51 -0500

    Day 8 solution and tests

--- lib/advent_of_code/solution/year_2023/day_08.ex
+++ lib/advent_of_code/solution/year_2023/day_08.ex
@@ -1,7 +1,74 @@
 defmodule AdventOfCode.Solution.Year2023.Day08 do
-  def part1(_input) do
+  alias AdventOfCode.Math
+
+  @type t :: %__MODULE__{
+          labels: %{(index :: non_neg_integer) => String.t()},
+          step: non_neg_integer,
+          zs: MapSet.t(step :: non_neg_integer)
+        }
+  @enforce_keys [:labels]
+  defstruct @enforce_keys ++ [step: 0, zs: MapSet.new()]
+
+  def new(labels) do
+    labels
+    |> Enum.with_index(fn l, i -> {i, l} end)
+    |> Map.new()
+    |> then(&%__MODULE__{labels: &1})
   end
 
-  def part2(_input) do
+  def part1(input) do
+    input
+    |> parse()
+    |> steps_to_z(["AAA"], &(&1 == "ZZZ"))
+  end
+
+  def part2(input) do
+    input
+    |> parse()
+    |> steps_to_z(&match?(<<_, _, ?Z>>, &1))
+  end
+
+  defp steps_to_z({dirs, map}, done?) do
+    steps_to_z({dirs, map}, for(<<_, _, ?A>> = label <- Map.keys(map), do: label), done?)
+  end
+
+  defp steps_to_z({dirs, map}, starting_labels, done?) do
+    dirs
+    |> Stream.cycle()
+    |> Enum.reduce_while(new(starting_labels), fn dir, acc ->
+      if map_size(acc.labels) == 0 do
+        {:halt, acc.zs}
+      else
+        {:cont, update_acc(acc, map, dir, done?)}
+      end
+    end)
+    |> Math.lcm()
+  end
+
+  defp update_acc(acc, map, dir, done?) do
+    found_z = Enum.flat_map(acc.labels, fn {i, l} -> if done?.(l), do: [i], else: [] end)
+
+    labels =
+      acc.labels
+      |> Map.drop(found_z)
+      |> Map.new(fn {i, l} -> {i, map[l][dir]} end)
+
+    zs = if match?([_ | _], found_z), do: MapSet.put(acc.zs, acc.step), else: acc.zs
+
+    %{acc | labels: labels, zs: zs, step: acc.step + 1}
+  end
+
+  defp parse(input) do
+    [dirs, map] = String.split(input, "\n\n")
+
+    dirs = for <<dir::1-bytes <- dirs>>, do: dir |> String.downcase() |> String.to_existing_atom()
+
+    map =
+      for <<label::3-bytes, _::4*8, left::3-bytes, _::2*8, right::3-bytes, _::bytes>> <-
+            String.split(map, "\n", trim: true),
+          into: %{},
+          do: {label, %{l: left, r: right}}
+
+    {dirs, map}
   end
 end
--- test/advent_of_code/solution/year_2023/day_08_test.exs
+++ test/advent_of_code/solution/year_2023/day_08_test.exs
@@ -5,22 +5,50 @@ defmodule AdventOfCode.Solution.Year2023.Day08Test do
 
   setup do
     [
-      input: """
+      input1: """
+      RL
+
+      AAA = (BBB, CCC)
+      BBB = (DDD, EEE)
+      CCC = (ZZZ, GGG)
+      DDD = (DDD, DDD)
+      EEE = (EEE, EEE)
+      GGG = (GGG, GGG)
+      ZZZ = (ZZZ, ZZZ)
+      """,
+      input2: """
+      LLR
+
+      AAA = (BBB, BBB)
+      BBB = (AAA, ZZZ)
+      ZZZ = (ZZZ, ZZZ)
+      """,
+      input3: """
+      LR
+
+      11A = (11B, XXX)
+      11B = (XXX, 11Z)
+      11Z = (11B, XXX)
+      22A = (22B, XXX)
+      22B = (22C, 22C)
+      22C = (22Z, 22Z)
+      22Z = (22B, 22B)
+      XXX = (XXX, XXX)
       """
     ]
   end
 
-  @tag :skip
-  test "part1", %{input: input} do
-    result = part1(input)
+  test "part1", %{input1: input1, input2: input2} do
+    result = part1(input1)
+    assert result == 2
 
-    assert result
+    result = part1(input2)
+    assert result == 6
   end
 
-  @tag :skip
-  test "part2", %{input: input} do
+  test "part2", %{input3: input} do
     result = part2(input)
 
-    assert result
+    assert result == 6
   end
 end
