b1fde7fc668d98db467d84619903bfed34e7c18a
Jon Zimbel <jzimbel@gmail.com>
2023-12-10 10:46:44 -0500

    No unnecessary parentheses on types!

--- lib/advent_of_code/math.ex
+++ lib/advent_of_code/math.ex
@@ -6,7 +6,7 @@ defmodule AdventOfCode.Math do
   @doc """
   Greatest common denominator.
   """
-  @spec gcd(Enumerable.t(pos_integer)) :: pos_integer()
+  @spec gcd(Enumerable.t(pos_integer)) :: pos_integer
   def gcd(ns), do: Enum.reduce(ns, &gcd/2)
 
   defp gcd(d, 0), do: d
@@ -15,7 +15,7 @@ defmodule AdventOfCode.Math do
   @doc """
   Least common multiple.
   """
-  @spec lcm(Enumerable.t(pos_integer)) :: pos_integer()
+  @spec lcm(Enumerable.t(pos_integer)) :: pos_integer
   def lcm(ns), do: Enum.reduce(ns, &lcm/2)
 
   defp lcm(a, b), do: div(a * b, gcd(a, b))
