a23c3738356ac0e2737f563dea2110e510ce46c5
Jon Zimbel <jzimbel@gmail.com>
2023-12-14 21:34:07 -0500

    Day 12 solution and tests

--- lib/advent_of_code/solution/year_2023/day_12.ex
+++ lib/advent_of_code/solution/year_2023/day_12.ex
@@ -1,7 +1,82 @@
 defmodule AdventOfCode.Solution.Year2023.Day12 do
-  def part1(_input) do
+  @table __MODULE__.MemoTable
+
+  def part1(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Stream.map(&parse_line/1)
+    |> solve()
   end
 
-  def part2(_input) do
+  def part2(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Stream.map(&parse_line/1)
+    |> Stream.map(&quintuple/1)
+    |> solve()
+  end
+
+  defp solve(parsed_lines) do
+    :ets.new(@table, [
+      :named_table,
+      :public,
+      read_concurrency: true,
+      write_concurrency: true,
+      decentralized_counters: true
+    ])
+
+    parsed_lines
+    |> Task.async_stream(fn {pat, sizes} -> count_arrangements(pat, sizes) end, ordered: false)
+    |> Stream.map(fn {:ok, count} -> count end)
+    |> Enum.sum()
+  after
+    :ets.delete(@table)
+  end
+
+  defp quintuple({pat, sizes}) do
+    {Enum.join(List.duplicate(pat, 5), "?"), for(_ <- 1..4, reduce: sizes, do: (l -> sizes ++ l))}
+  end
+
+  defp count_arrangements(pat, sizes, offset \\ 0) do
+    memo({pat, sizes, offset}, fn -> do_count(pat, sizes, offset) end)
+  end
+
+  defp do_count(pat, [size | _], offset) when byte_size(pat) - offset < size, do: 0
+
+  defp do_count(pat, [], offset) do
+    # If there are any '#'s in the remainder of the string, this arrangement is not valid.
+    if :binary.match(pat, "#", scope: end_scope(pat, offset)) == :nomatch, do: 1, else: 0
+  end
+
+  defp do_count(pat, [size | sizes], offset) do
+    {segment_size, dot} = if match?([_ | _], sizes), do: {size + 1, "[?.]"}, else: {size, ""}
+    pmax = with {pos, _len} <- :binary.match(pat, "#", scope: end_scope(pat, offset)), do: pos
+
+    ~r"""
+    [?\#]   # This is the character that we'll get the index of. The first character of this valid contiguous section.
+    (?=     # Positive lookahead. Check that the correct characters follow the start of this contiguous section, without consuming them.
+      [?\#]{#{size - 1}}   # Remaining characters of the contiguous section.
+      #{dot}                # Optional trailing '.', if this is not the final contiguous section.
+    )
+    """x
+    |> Regex.scan(pat, offset: offset, return: :index)
+    |> Enum.reduce(0, fn
+      [{pos, _}], sum when pos <= pmax -> sum + count_arrangements(pat, sizes, pos + segment_size)
+      _, sum -> sum
+    end)
+  end
+
+  defp end_scope(pat, offset), do: {byte_size(pat), offset - byte_size(pat)}
+
+  defp memo(key, fun) do
+    case :ets.match(@table, {key, :"$1"}) do
+      [[value]] -> value
+      [] -> tap(fun.(), &:ets.insert(@table, {key, &1}))
+    end
+  end
+
+  defp parse_line(line) do
+    [springs_str, sizes_str] = String.split(line)
+    {springs_str, Enum.map(String.split(sizes_str, ","), &String.to_integer/1)}
   end
 end
--- test/advent_of_code/solution/year_2023/day_12_test.exs
+++ test/advent_of_code/solution/year_2023/day_12_test.exs
@@ -1,26 +1,33 @@
 defmodule AdventOfCode.Solution.Year2023.Day12Test do
-  use ExUnit.Case, async: true
+  use ExUnit.Case, async: false
 
   import AdventOfCode.Solution.Year2023.Day12
 
   setup do
     [
       input: """
+      ???.### 1,1,3
+      .??..??...?##. 1,1,3
+      ?#?#?#?#?#?#?#? 1,3,1,6
+      ????.#...#... 4,1,1
+      ????.######..#####. 1,6,5
+      ?###???????? 3,2,1
       """
     ]
   end
 
-  @tag :skip
   test "part1", %{input: input} do
     result = part1(input)
 
-    assert result
+    assert result == 21
   end
 
-  @tag :skip
   test "part2", %{input: input} do
     result = part2(input)
+    assert result == 525_152
 
-    assert result
+    input_split = String.split(input, "\n", trim: true)
+    result_split = Enum.map(input_split, &part2/1)
+    assert result_split == [1, 16384, 1, 16, 2500, 506_250]
   end
 end
