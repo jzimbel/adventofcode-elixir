e3e1e4f5ba9d1b48df9086af2fd2e6f79ec881c7
Jon Zimbel <jzimbel@gmail.com>
2023-12-19 15:57:51 -0500

    Day 15 solution and tests

--- lib/advent_of_code/solution/year_2023/day_15.ex
+++ lib/advent_of_code/solution/year_2023/day_15.ex
@@ -1,7 +1,24 @@
 defmodule AdventOfCode.Solution.Year2023.Day15 do
-  def part1(_input) do
+  def part1(input), do: input |> parse() |> Stream.map(&hash/1) |> Enum.sum()
+  def part2(input), do: input |> parse() |> to_map() |> stream_focusing_powers() |> Enum.sum()
+
+  defp to_map(ops) do
+    ops
+    |> Stream.map(&Regex.run(~r/([^=]+)(?:-|=(.+))/, &1, capture: :all_but_first))
+    |> Enum.reduce(Map.new(0..255, &{&1, []}), fn
+      [i, f], m -> Map.update!(m, hash(i), &List.keystore(&1, i, 0, {i, String.to_integer(f)}))
+      [i], m -> Map.update!(m, hash(i), &List.keydelete(&1, i, 0))
+    end)
   end
 
-  def part2(_input) do
+  defp stream_focusing_powers(map) do
+    Stream.flat_map(map, fn {box, lenses} ->
+      lenses
+      |> Stream.with_index(1)
+      |> Stream.map(fn {{_label, f}, slot} -> (1 + box) * slot * f end)
+    end)
   end
+
+  defp hash(str), do: for(<<char <- str>>, reduce: 0, do: (acc -> rem((acc + char) * 17, 256)))
+  defp parse(input), do: input |> String.split(",", trim: true) |> Stream.map(&String.trim/1)
 end
--- test/advent_of_code/solution/year_2023/day_15_test.exs
+++ test/advent_of_code/solution/year_2023/day_15_test.exs
@@ -6,21 +6,20 @@ defmodule AdventOfCode.Solution.Year2023.Day15Test do
   setup do
     [
       input: """
+      rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7
       """
     ]
   end
 
-  @tag :skip
   test "part1", %{input: input} do
     result = part1(input)
 
-    assert result
+    assert result == 1320
   end
 
-  @tag :skip
   test "part2", %{input: input} do
     result = part2(input)
 
-    assert result
+    assert result == 145
   end
 end
