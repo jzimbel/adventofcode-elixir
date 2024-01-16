b86b36ede4302a0cf3c72891074d504b018d29c3
Jon Zimbel <jzimbel@gmail.com>
2024-01-15 20:42:37 -0500

    Day 22 solution and tests

--- lib/advent_of_code/solution/year_2023/day_22.ex
+++ lib/advent_of_code/solution/year_2023/day_22.ex
@@ -1,7 +1,137 @@
 defmodule AdventOfCode.Solution.Year2023.Day22 do
-  def part1(_input) do
+  defmodule Brick do
+    @enforce_keys [:x, :y, :z, :footprint]
+    defstruct @enforce_keys ++ [dropped?: false]
+
+    def new(line) do
+      [from, to] = String.split(line, "~")
+      [x1, y1, z1] = for n <- String.split(from, ","), do: String.to_integer(n)
+      [x2, y2, z2] = for n <- String.split(to, ","), do: String.to_integer(n)
+
+      x = x1..x2//1
+      y = y1..y2//1
+      %__MODULE__{x: x, y: y, z: z1..z2//1, footprint: footprint(x, y)}
+    end
+
+    defp footprint(x_range, y_range), do: for(x <- x_range, y <- y_range, do: {x, y})
+
+    def drop_to_z(b, z) when z == b.z.first, do: %{b | dropped?: true}
+    def drop_to_z(b, z), do: %{b | z: Range.shift(b.z, z - b.z.first), dropped?: true}
+
+    def lt?(b1, b2), do: b1.z.last < b2.z.first
   end
 
-  def part2(_input) do
+  require AdventOfCode.Counter.Single, as: Counter
+
+  @counter __MODULE__.Counter
+
+  def part1(input) do
+    {bricks, columns} = init(input)
+
+    load_bearing_bricks =
+      bricks
+      |> Stream.map(fn {id, brick} ->
+        supporting_brick_ids =
+          brick.footprint
+          |> Stream.map(&adjacent_below(columns[&1], id))
+          |> Stream.reject(&is_nil/1)
+          |> Stream.filter(fn below_id -> bricks[below_id].z.last + 1 == brick.z.first end)
+          |> Enum.uniq()
+
+        case supporting_brick_ids do
+          [single_supporting_brick] -> single_supporting_brick
+          _ -> nil
+        end
+      end)
+      |> Stream.reject(&is_nil/1)
+      |> MapSet.new()
+
+    map_size(bricks) - MapSet.size(load_bearing_bricks)
+  end
+
+  # Note: Slow! Takes about 1:00, or 3:00+ without the Task.async_stream.
+  # Probably faster way: graph theory!
+  # After dropping all bricks, build a directed dependency graph: brick A -> brick B, on which A rests
+  # For each brick A, find paths from it to the bottom. If all pass through some brick B, then B is load-bearing and would cause A to fall if removed.
+  # For each brick, count number of load-bearing bricks. Sum is the solution.
+  def part2(input) do
+    {bricks, columns} = init(input)
+    bricks = Map.new(bricks, fn {id, brick} -> {id, %{brick | dropped?: false}} end)
+
+    Counter.start_link([], name: @counter)
+
+    bricks
+    |> Task.async_stream(__MODULE__, :simulate_removal, [bricks, columns], ordered: false)
+    |> Stream.run()
+
+    Counter.value(@counter)
+  after
+    Counter.stop(@counter)
+  end
+
+  def simulate_removal({id, brick}, bricks, columns) do
+    _ = drop_all(Map.delete(bricks, id), delete_from_columns(columns, id, brick))
+    nil
+  end
+
+  defp init(input) do
+    bricks =
+      input
+      |> String.split("\n", trim: true)
+      |> Stream.with_index()
+      |> Map.new(fn {line, id} -> {id, Brick.new(line)} end)
+
+    columns =
+      for {id, brick} <- bricks, reduce: %{} do
+        acc ->
+          for xy <- brick.footprint, reduce: acc do
+            acc -> Map.update(acc, xy, [id], &insert_brick(&1, id, bricks))
+          end
+      end
+
+    bricks = drop_all(bricks, columns)
+
+    {bricks, columns}
+  end
+
+  defp delete_from_columns(columns, id, brick) do
+    for xy <- brick.footprint, reduce: columns do
+      cols -> update_in(cols[xy], &List.delete(&1, id))
+    end
+  end
+
+  defp drop_all(bricks, columns) do
+    lowest_undropped_brick =
+      bricks
+      |> Stream.reject(fn {_, brick} -> brick.dropped? end)
+      |> Enum.min_by(fn {_, brick} -> brick.z.first end, fn -> :all_dropped end)
+
+    case lowest_undropped_brick do
+      {id, brick} ->
+        highest_z_below =
+          brick.footprint
+          |> Stream.map(&adjacent_below(columns[&1], id))
+          |> Stream.reject(&is_nil/1)
+          |> Stream.map(&bricks[&1].z.last)
+          |> Enum.max(fn -> 0 end)
+
+        brick
+        |> Brick.drop_to_z(highest_z_below + 1)
+        |> tap(&(&1.z.first < brick.z.first and Counter.increment(@counter)))
+        |> then(&drop_all(%{bricks | id => &1}, columns))
+
+      :all_dropped ->
+        bricks
+    end
+  end
+
+  defp adjacent_below([id | _], id), do: nil
+  defp adjacent_below([adjacent_id, id | _], id), do: adjacent_id
+  defp adjacent_below(column, id), do: adjacent_below(tl(column), id)
+
+  defp insert_brick(column, new_id, bricks) do
+    new_brick = bricks[new_id]
+    {l, r} = Enum.split_while(column, &Brick.lt?(bricks[&1], new_brick))
+    l ++ [new_id | r]
   end
 end
--- test/advent_of_code/solution/year_2023/day_22_test.exs
+++ test/advent_of_code/solution/year_2023/day_22_test.exs
@@ -6,21 +6,41 @@ defmodule AdventOfCode.Solution.Year2023.Day22Test do
   setup do
     [
       input: """
+      1,0,1~1,2,1
+      0,0,2~2,0,2
+      0,2,3~2,2,3
+      0,0,4~0,2,4
+      2,0,5~2,2,5
+      0,1,6~2,1,6
+      1,1,8~1,1,9
+      """,
+      edge_case: """
+      1,0,1~1,2,1
+      0,0,2~2,0,2
+      0,2,3~2,2,3
+      0,0,4~0,2,4
+      2,0,5~2,2,5
+      0,1,6~2,1,6
+      1,1,8~1,1,9
+      1,1,10~1,3,10
+      1,2,11~1,4,11
       """
     ]
   end
 
-  @tag :skip
-  test "part1", %{input: input} do
+  test "part1", %{input: input, edge_case: edge_case} do
     result = part1(input)
 
-    assert result
+    assert result == 5
+
+    result = part1(edge_case)
+
+    assert result == 5
   end
 
-  @tag :skip
   test "part2", %{input: input} do
     result = part2(input)
 
-    assert result
+    assert result == 7
   end
 end
