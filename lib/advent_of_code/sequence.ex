b6ad9da4e7350eaf74dba3670903db2ce88ed03c
Jon Zimbel <jzimbel@gmail.com>
2024-03-12 23:46:12 -0400

    Fix function doc

--- lib/advent_of_code/sequence.ex
+++ lib/advent_of_code/sequence.ex
@@ -5,12 +5,12 @@ defmodule AdventOfCode.Sequence do
 
   @doc """
   Given an indexed list describing the repeating segment of an infinitely repeating sequence,
-  returns the value at `at_index` of the sequence.
+  returns the value at index `i` of the sequence.
 
   Indices in the list do not need to start at 0, but they do need to be contiguous,
   and the list must be ordered by index, ascending.
 
-  `at_index` can be any integer.
+  `i` can be any integer.
   """
   @spec at(nonempty_list({a, index :: integer}), integer) :: a when a: term
   def at(repeat, i) do
