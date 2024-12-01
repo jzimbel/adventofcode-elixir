746072901565715bac37265bdb3f60543817d414
Jon Zimbel <jzimbel@gmail.com>
2024-12-01 13:26:46 -0500

    Day 1 solution and tests

--- lib/advent_of_code/solution/year_2024/day_01.ex
+++ lib/advent_of_code/solution/year_2024/day_01.ex
@@ -1,7 +1,33 @@
 defmodule AdventOfCode.Solution.Year2024.Day01 do
-  def part1(_input) do
+  use AdventOfCode.Solution.SharedParse
+
+  @impl true
+  def parse(input) do
+    input
+    |> String.split()
+    |> Enum.map(&String.to_integer/1)
+    |> then(
+      &[
+        _col_a = Enum.take_every(&1, 2),
+        _col_b = Enum.take_every(tl(&1), 2)
+      ]
+    )
   end
 
-  def part2(_input) do
+  def part1(cols) do
+    cols
+    |> Enum.map(&Enum.sort/1)
+    |> Enum.zip()
+    |> Enum.map(fn {a, b} -> abs(a - b) end)
+    |> Enum.sum()
+  end
+
+  def part2([col_a, col_b]) do
+    a_freq = Enum.frequencies(col_a)
+    b_freq = Enum.frequencies(col_b)
+
+    a_freq
+    |> Enum.map(fn {n, freq} -> n * freq * Map.get(b_freq, n, 0) end)
+    |> Enum.sum()
   end
 end
--- test/advent_of_code/solution/year_2024/day_01_test.exs
+++ test/advent_of_code/solution/year_2024/day_01_test.exs
@@ -6,21 +6,31 @@ defmodule AdventOfCode.Solution.Year2024.Day01Test do
   setup do
     [
       input: """
+      3   4
+      4   3
+      2   5
+      1   3
+      3   9
+      3   3
       """
     ]
   end
 
-  @tag :skip
-  test "part1", %{input: input} do
-    result = part1(input)
+  test "parse", %{input: input} do
+    cols = parse(input)
 
-    assert result
+    assert [[3, 4, 2, 1, 3, 3], [4, 3, 5, 3, 9, 3]] == cols
   end
 
-  @tag :skip
-  test "part2", %{input: input} do
-    result = part2(input)
+  test "part1", %{input: input} do
+    result = input |> parse() |> part1()
 
-    assert result
+    assert result == 11
+  end
+
+  test "part2", %{input: input} do
+    result = input |> parse() |> part2()
+
+    assert result == 31
   end
 end
