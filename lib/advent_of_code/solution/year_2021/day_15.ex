7981b55e0baf83789def120713e8274b24479de5
Jon Zimbel <jzimbel@gmail.com>
2024-12-01 13:38:01 -0500

    Reset year 2021 day 15 solution to remove libgraph dep (it didn't work anyway...)

--- lib/advent_of_code/solution/year_2021/day_15.ex
+++ lib/advent_of_code/solution/year_2021/day_15.ex
@@ -1,60 +1,7 @@
 defmodule AdventOfCode.Solution.Year2021.Day15 do
-  alias AdventOfCode.Grid
-  alias Graph
-
-  def part1(input) do
-    input
-    |> Grid.from_input()
-    |> make_numeric()
-    |> shortest_path_weight()
+  def part1(_input) do
   end
 
-  def part2(input) do
-    input
-    |> Grid.from_input()
-    |> make_numeric()
-    |> embiggen()
-    |> shortest_path_weight()
-  end
-
-  defp shortest_path_weight(grid) do
-    grid
-    |> build_graph()
-    |> Graph.dijkstra({0, 0}, {grid.width - 1, grid.height - 1})
-    |> Enum.drop(1)
-    |> Enum.map(&Grid.at(grid, &1))
-    |> Enum.sum()
-  end
-
-  defp build_graph(grid) do
-    Graph.new()
-    |> Graph.add_edges(
-      Stream.flat_map(grid.grid, fn {coords, _} ->
-        grid
-        |> Grid.adjacent_cells(coords, :cardinal)
-        |> Enum.map(fn {neighbor_coords, neighbor_value} ->
-          {coords, neighbor_coords, weight: neighbor_value}
-        end)
-      end)
-    )
-  end
-
-  defp embiggen(grid) do
-    grid
-    |> Grid.to_list()
-    |> Stream.flat_map(fn {{x, y}, value} ->
-      for x_offset <- 0..4,
-          y_offset <- 0..4 do
-        {
-          {x + grid.width * x_offset, y + grid.height * y_offset},
-          rem(value + x_offset + y_offset - 1, 9) + 1
-        }
-      end
-    end)
-    |> then(&%Grid{grid: Enum.into(&1, %{}), width: grid.width * 5, height: grid.height * 5})
-  end
-
-  defp make_numeric(grid) do
-    Grid.map(grid, fn {_, char_value} -> char_value - ?0 end)
+  def part2(_input) do
   end
 end
--- test/advent_of_code/solution/year_2021/day_15_test.exs
+++ test/advent_of_code/solution/year_2021/day_15_test.exs
@@ -6,29 +6,21 @@ defmodule AdventOfCode.Solution.Year2021.Day15Test do
   setup do
     [
       input: """
-      1163751742
-      1381373672
-      2136511328
-      3694931569
-      7463417111
-      1319128137
-      1359912421
-      3125421639
-      1293138521
-      2311944581
       """
     ]
   end
 
+  @tag :skip
   test "part1", %{input: input} do
     result = part1(input)
 
-    assert result == 40
+    assert result
   end
 
+  @tag :skip
   test "part2", %{input: input} do
     result = part2(input)
 
-    assert result == 315
+    assert result
   end
 end
