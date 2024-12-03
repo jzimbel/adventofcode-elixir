aecece82ed52c502615a83c59861f49c1bb9e37b
Jon Zimbel <jzimbel@gmail.com>
2024-12-02 21:34:29 -0500

    Day 2 solution and tests

--- lib/advent_of_code/solution/year_2024/day_02.ex
+++ lib/advent_of_code/solution/year_2024/day_02.ex
@@ -1,7 +1,44 @@
 defmodule AdventOfCode.Solution.Year2024.Day02 do
-  def part1(_input) do
+  use AdventOfCode.Solution.SharedParse
+
+  @impl true
+  def parse(input) do
+    for line <- String.split(input, "\n", trim: true) do
+      line |> String.split() |> Enum.map(&String.to_integer/1)
+    end
   end
 
-  def part2(_input) do
+  def part1(reports), do: Enum.count(reports, &safe_p1?/1)
+  def part2(reports), do: Enum.count(reports, &safe_p2?/1)
+
+  defp safe_p1?(ns) do
+    ns
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Stream.scan(nil, &check_interval/2)
+    |> Enum.all?(&(&1 != :not_safe))
+  end
+
+  defp check_interval([a, b], asc?) do
+    with true <- gradual?(a, b),
+         {true, asc?} <- asc_desc?(a, b, asc?) do
+      asc?
+    else
+      false -> :not_safe
+    end
+  end
+
+  defp gradual?(a, b), do: abs(b - a) in 1..3
+
+  defp asc_desc?(a, b, nil), do: {true, b - a > 0}
+  defp asc_desc?(a, b, asc?) when b - a > 0 == asc?, do: {true, asc?}
+  defp asc_desc?(_a, _b, _asc?), do: false
+
+  # Brute-force runs in a few hundred µs so I guess it's fine!
+  defp safe_p2?(ns) do
+    safe_p1?(ns) or
+      Enum.any?(
+        0..(length(ns) - 1),
+        &safe_p1?(List.delete_at(ns, &1))
+      )
   end
 end
--- test/advent_of_code/solution/year_2024/day_02_test.exs
+++ test/advent_of_code/solution/year_2024/day_02_test.exs
@@ -6,21 +6,25 @@ defmodule AdventOfCode.Solution.Year2024.Day02Test do
   setup do
     [
       input: """
+      7 6 4 2 1
+      1 2 7 8 9
+      9 7 6 2 1
+      1 3 2 4 5
+      8 6 4 4 1
+      1 3 6 7 9
       """
     ]
   end
 
-  @tag :skip
   test "part1", %{input: input} do
-    result = part1(input)
+    result = input |> parse() |> part1()
 
-    assert result
+    assert result == 2
   end
 
-  @tag :skip
   test "part2", %{input: input} do
-    result = part2(input)
+    result = input |> parse() |> part2()
 
-    assert result
+    assert result == 4
   end
 end
