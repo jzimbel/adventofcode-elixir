3db73be096186f53e3db1381fa7ee8acfb11dbc2
Jon Zimbel <jzimbel@gmail.com>
2024-12-04 20:43:40 -0500

    Day 3 solution and tests

--- lib/advent_of_code/solution/year_2024/day_03.ex
+++ lib/advent_of_code/solution/year_2024/day_03.ex
@@ -1,7 +1,41 @@
 defmodule AdventOfCode.Solution.Year2024.Day03 do
-  def part1(_input) do
+  use AdventOfCode.Solution.SharedParse
+
+  @impl true
+  def parse(input), do: String.trim(input)
+
+  def part1(mem) do
+    ~r/mul\((\d{1,3}),(\d{1,3})\)/
+    |> Regex.scan(mem, capture: :all_but_first)
+    |> sum_of_products()
   end
 
-  def part2(_input) do
+  def part2(mem) do
+    # (?| ...) drops unmatched alternatives from the returned captures.
+    # E.g. matching "don't()" will produce ["don't()"] instead of ["", "", "", "don't()"]
+    ~r"""
+    (?|(mul)\((\d{1,3}),(\d{1,3})\)
+      |(do)\(\)
+      |(don't)\(\)
+    )
+    """x
+    |> Regex.scan(mem, capture: :all_but_first)
+    |> apply_toggles()
+    |> sum_of_products()
+  end
+
+  defp sum_of_products(factor_pairs) do
+    factor_pairs
+    |> Enum.map(fn [a, b] -> String.to_integer(a) * String.to_integer(b) end)
+    |> Enum.sum()
+  end
+
+  defp apply_toggles(matches) do
+    Enum.reduce(matches, %{on?: true, acc: []}, fn
+      ["mul" | factor_pair], state when state.on? -> update_in(state.acc, &[factor_pair | &1])
+      ["mul" | _factor_pair], state when not state.on? -> state
+      ["do"], state -> %{state | on?: true}
+      ["don't"], state -> %{state | on?: false}
+    end).acc
   end
 end
--- test/advent_of_code/solution/year_2024/day_03_test.exs
+++ test/advent_of_code/solution/year_2024/day_03_test.exs
@@ -5,22 +5,24 @@ defmodule AdventOfCode.Solution.Year2024.Day03Test do
 
   setup do
     [
-      input: """
+      input1: """
+      xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+      """,
+      input2: """
+      xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
       """
     ]
   end
 
-  @tag :skip
-  test "part1", %{input: input} do
-    result = part1(input)
+  test "part1", %{input1: input} do
+    result = input |> parse() |> part1()
 
-    assert result
+    assert result == 161
   end
 
-  @tag :skip
-  test "part2", %{input: input} do
-    result = part2(input)
+  test "part2", %{input2: input} do
+    result = input |> parse() |> part2()
 
-    assert result
+    assert result == 48
   end
 end
