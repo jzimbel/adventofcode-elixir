647b762aa0d4438c7ee48c634c0b784cf22213c2
Jon Zimbel <jzimbel@gmail.com>
2024-12-06 12:15:32 -0500

    Day 5 solution and tests

--- lib/advent_of_code/solution/year_2024/day_05.ex
+++ lib/advent_of_code/solution/year_2024/day_05.ex
@@ -1,7 +1,67 @@
 defmodule AdventOfCode.Solution.Year2024.Day05 do
-  def part1(_input) do
+  use AdventOfCode.Solution.SharedParse
+
+  @typep page :: integer
+
+  @impl true
+  @spec parse(String.t()) :: {
+          # k comes before all pages in v
+          %{page => MapSet.t(page)},
+          [[page]]
+        }
+  def parse(input) do
+    [rules, updates] = String.split(input, "\n\n", trim: true)
+    {parse_afters(rules), parse_updates(updates)}
   end
 
-  def part2(_input) do
+  defp parse_afters(rules) do
+    for rule <- String.split(rules, "\n", trim: true), reduce: %{} do
+      afters ->
+        [before, after_] = for page <- String.split(rule, "|"), do: String.to_integer(page)
+
+        afters
+        |> Map.update(before, MapSet.new([after_]), &MapSet.put(&1, after_))
+        # Adds an entry for the last page (which has no afters)
+        |> Map.put_new(after_, MapSet.new())
+    end
+  end
+
+  defp parse_updates(updates) do
+    for update <- String.split(updates, "\n", trim: true) do
+      for page <- String.split(update, ",") do
+        String.to_integer(page)
+      end
+    end
+  end
+
+  def part1({afters, updates}) do
+    for(update <- updates, ordered?(update, afters), do: update)
+    |> sum_middles()
+  end
+
+  def part2({afters, updates}) do
+    for(update <- updates, sorted = sorted_or_nil(update, afters), sorted != nil, do: sorted)
+    |> sum_middles()
+  end
+
+  defp ordered?(update, afters) do
+    update
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.all?(&page_pair_ordered?(&1, afters))
+  end
+
+  defp sorted_or_nil(update, afters) do
+    case Enum.sort(update, &page_pair_ordered?([&1, &2], afters)) do
+      ^update -> nil
+      sorted -> sorted
+    end
+  end
+
+  defp page_pair_ordered?([l, r], afters), do: r in afters[l]
+
+  defp sum_middles(updates) do
+    for update <- updates, reduce: 0 do
+      acc -> acc + Enum.at(update, div(length(update), 2))
+    end
   end
 end
--- test/advent_of_code/solution/year_2024/day_05_test.exs
+++ test/advent_of_code/solution/year_2024/day_05_test.exs
@@ -6,21 +6,48 @@ defmodule AdventOfCode.Solution.Year2024.Day05Test do
   setup do
     [
       input: """
+      47|53
+      97|13
+      97|61
+      97|47
+      75|29
+      61|13
+      75|53
+      29|13
+      97|29
+      53|29
+      61|53
+      97|53
+      61|29
+      47|13
+      75|47
+      97|75
+      47|61
+      75|61
+      47|29
+      75|13
+      53|13
+
+      75,47,61,53,29
+      97,61,53,29,13
+      75,29,13
+      75,97,47,61,53
+      61,13,29
+      97,13,75,29,47
       """
     ]
   end
 
-  @tag :skip
   test "part1", %{input: input} do
-    result = part1(input)
+    result = input |> parse() |> part1()
 
-    assert result
+    assert result == 143
   end
 
   @tag :skip
   test "part2", %{input: input} do
-    result = part2(input)
+    result = input |> parse() |> part2()
 
-    assert result
+    assert result == 123
   end
 end
