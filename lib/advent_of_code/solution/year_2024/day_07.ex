867ff481891e56eee6253844bc4bd05fc039821b
Jon Zimbel <jzimbel@gmail.com>
2024-12-08 17:02:15 -0500

    A nearly inscrutable day 7 solution, and tests

--- lib/advent_of_code/solution/year_2024/day_07.ex
+++ lib/advent_of_code/solution/year_2024/day_07.ex
@@ -1,7 +1,49 @@
 defmodule AdventOfCode.Solution.Year2024.Day07 do
-  def part1(_input) do
+  use AdventOfCode.Solution.SharedParse
+
+  @impl true
+  def parse(input) do
+    for line <- String.split(input, "\n", trim: true) do
+      [target, ns] = String.split(line, ":")
+      {String.to_integer(target), for(n <- String.split(ns), do: String.to_integer(n))}
+    end
   end
 
-  def part2(_input) do
+  def part1(equations), do: solve(equations, 2)
+  def part2(equations), do: solve(equations, 3)
+
+  defp solve(eqs, n_ops) do
+    for {target, _ns} = eq <- eqs, solvable?(eq, n_ops), reduce: 0, do: (acc -> acc + target)
   end
+
+  def solvable?({target, ns}, n_ops) do
+    ns
+    |> stream_calculations(n_ops)
+    |> Enum.any?(&(&1 == target))
+  end
+
+  def stream_calculations([], _n_ops), do: []
+  def stream_calculations([n], _n_ops), do: [n]
+
+  def stream_calculations(ns, n_ops) do
+    upper_bound = n_ops ** (length(ns) - 1)
+
+    Stream.unfold(0, fn
+      ^upper_bound -> nil
+      op_combo -> {apply_ops(op_combo, ns, n_ops), op_combo + 1}
+    end)
+  end
+
+  defp apply_ops(op_combo, ns, n_ops) do
+    op_combo
+    |> Integer.digits(n_ops)
+    |> zero_pad(length(ns))
+    |> Enum.zip_reduce(ns, 0, &eval_op/3)
+  end
+
+  defp zero_pad(l, n), do: List.duplicate(0, max(0, n - length(l))) ++ l
+
+  defp eval_op(0, b, a), do: a + b
+  defp eval_op(1, b, a), do: a * b
+  defp eval_op(2, b, a), do: a * 10 ** (1 + trunc(:math.log10(b))) + b
 end
--- test/advent_of_code/solution/year_2024/day_07_test.exs
+++ test/advent_of_code/solution/year_2024/day_07_test.exs
@@ -6,21 +6,28 @@ defmodule AdventOfCode.Solution.Year2024.Day07Test do
   setup do
     [
       input: """
+      190: 10 19
+      3267: 81 40 27
+      83: 17 5
+      156: 15 6
+      7290: 6 8 6 15
+      161011: 16 10 13
+      192: 17 8 14
+      21037: 9 7 18 13
+      292: 11 6 16 20
       """
     ]
   end
 
-  @tag :skip
   test "part1", %{input: input} do
-    result = part1(input)
+    result = input |> parse() |> part1()
 
-    assert result
+    assert result == 3749
   end
 
-  @tag :skip
   test "part2", %{input: input} do
-    result = part2(input)
+    result = input |> parse() |> part2()
 
-    assert result
+    assert result == 11387
   end
 end
