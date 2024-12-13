49e6c6d1f14eced5f250a285206fe6e1b15692c3
Jon Zimbel <jzimbel@gmail.com>
2024-12-13 08:59:09 -0500

    Day 10 solution and tests

--- lib/advent_of_code/solution/year_2024/day_10.ex
+++ lib/advent_of_code/solution/year_2024/day_10.ex
@@ -1,7 +1,35 @@
 defmodule AdventOfCode.Solution.Year2024.Day10 do
-  def part1(_input) do
+  use AdventOfCode.Solution.SharedParse
+
+  alias AdventOfCode.Grid, as: G
+
+  @impl true
+  def parse(input), do: G.from_input(input)
+
+  def part1(grid), do: solve(grid, true)
+  def part2(grid), do: solve(grid, false)
+
+  defp solve(grid, uniq?) do
+    for {point, ?0} <- grid, reduce: 0 do
+      acc -> acc + count_trailheads([point], ?1, grid, uniq?)
+    end
   end
 
-  def part2(_input) do
+  defp count_trailheads(points, ?9, grid, uniq?) do
+    length(next(points, ?9, grid, uniq?))
+  end
+
+  defp count_trailheads(points, target, grid, uniq?) do
+    count_trailheads(next(points, target, grid, uniq?), target + 1, grid, uniq?)
+  end
+
+  defp next(points, target, grid, uniq?) do
+    next_points =
+      for point <- points,
+          {next_point, ^target} <- G.adjacent_cells(grid, point, :cardinal) do
+        next_point
+      end
+
+    if uniq?, do: Enum.uniq(next_points), else: next_points
   end
 end
--- test/advent_of_code/solution/year_2024/day_10_test.exs
+++ test/advent_of_code/solution/year_2024/day_10_test.exs
@@ -5,22 +5,84 @@ defmodule AdventOfCode.Solution.Year2024.Day10Test do
 
   setup do
     [
-      input: """
+      input1: """
+      0123
+      1234
+      8765
+      9876
+      """,
+      input2: """
+      ...0...
+      ...1...
+      ...2...
+      6543456
+      7.....7
+      8.....8
+      9.....9
+      """,
+      input3: """
+      ..90..9
+      ...1.98
+      ...2..7
+      6543456
+      765.987
+      876....
+      987....
+      """,
+      input4: """
+      .....0.
+      ..4321.
+      ..5..2.
+      ..6543.
+      ..7..4.
+      ..8765.
+      ..9....
+      """,
+      input5: """
+      ..90..9
+      ...1.98
+      ...2..7
+      6543456
+      765.987
+      876....
+      987....
+      """,
+      input6: """
+      012345
+      123456
+      234567
+      345678
+      4.6789
+      56789.
+      """,
+      input7: """
+      89010123
+      78121874
+      87430965
+      96549874
+      45678903
+      32019012
+      01329801
+      10456732
       """
     ]
   end
 
-  @tag :skip
-  test "part1", %{input: input} do
-    result = part1(input)
+  test "part1", ctx do
+    result = ctx.input1 |> parse() |> part1()
+    assert result == 1
 
-    assert result
+    result = ctx.input2 |> parse() |> part1()
+    assert result == 2
+
+    result = ctx.input3 |> parse() |> part1()
+    assert result == 4
   end
 
-  @tag :skip
-  test "part2", %{input: input} do
-    result = part2(input)
-
-    assert result
+  test "part2", ctx do
+    assert ctx.input4 |> parse() |> part2() == 3
+    assert ctx.input5 |> parse() |> part2() == 13
+    assert ctx.input6 |> parse() |> part2() == 227
+    assert ctx.input7 |> parse() |> part2() == 81
   end
 end
