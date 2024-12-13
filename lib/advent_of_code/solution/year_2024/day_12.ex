94a17b6242546c7839d56249d72b6b6e3ddbd650
Jon Zimbel <jzimbel@gmail.com>
2024-12-13 09:02:21 -0500

    Day 12 solution and tests

--- lib/advent_of_code/solution/year_2024/day_12.ex
+++ lib/advent_of_code/solution/year_2024/day_12.ex
@@ -1,7 +1,96 @@
 defmodule AdventOfCode.Solution.Year2024.Day12 do
-  def part1(_input) do
+  use AdventOfCode.Solution.SharedParse
+
+  alias AdventOfCode.Grid, as: G
+
+  @impl true
+  def parse(input) do
+    grid = G.from_input(input)
+    {grid, regions(grid)}
   end
 
-  def part2(_input) do
+  def part1({grid, rs}) do
+    rs
+    |> Enum.map(&price_p1(&1, grid))
+    |> Enum.sum()
   end
+
+  def part2({grid, rs}) do
+    rs
+    |> Enum.map(&price_p2(&1, grid))
+    |> Enum.sum()
+  end
+
+  defp regions(grid) do
+    Enum.reduce(grid, %{regions: [], visited: MapSet.new()}, fn {coords, char}, acc ->
+      if coords in acc.visited do
+        acc
+      else
+        r = region(coords, char, grid)
+        %{acc | regions: [r | acc.regions], visited: MapSet.union(acc.visited, r)}
+      end
+    end).regions
+  end
+
+  defp region(coords, char, grid) do
+    region([coords], char, grid, MapSet.new())
+  end
+
+  defp region([], _, _, r), do: r
+
+  defp region(fringe, char, grid, r) do
+    next_fringe =
+      for coords <- fringe,
+          {next_coords, ^char} <- G.adjacent_cells(grid, coords, :cardinal),
+          next_coords not in fringe,
+          next_coords not in r,
+          uniq: true,
+          do: next_coords
+
+    region(next_fringe, char, grid, MapSet.union(r, MapSet.new(fringe)))
+  end
+
+  defp price_p1(r, grid), do: MapSet.size(r) * perimeter(r, grid)
+  defp price_p2(r, grid), do: MapSet.size(r) * side_count(r, grid)
+
+  defp perimeter(r, grid) do
+    r
+    |> Enum.map(fn coords ->
+      Enum.count(G.adjacent_coords(grid, coords, :cardinal, false), &(&1 not in r))
+    end)
+    |> Enum.sum()
+  end
+
+  defp side_count(r, grid) do
+    r
+    |> Enum.flat_map(fn coords ->
+      grid
+      |> G.adjacent_coords(coords, :cardinal, false)
+      |> Enum.reject(&(&1 in r))
+      |> Enum.map(&{coords, &1})
+    end)
+    |> Enum.group_by(
+      fn {from, to} -> subtract(to, from) end,
+      fn {from, _to} -> from end
+    )
+    |> Enum.flat_map(fn {heading, fences} ->
+      case heading do
+        {_, 0} -> Enum.group_by(fences, fn {x, _y} -> x end, fn {_x, y} -> y end)
+        {0, _} -> Enum.group_by(fences, fn {_x, y} -> y end, fn {x, _y} -> x end)
+      end
+      |> Enum.flat_map(fn {_, col_or_row} -> chunk_contiguous(col_or_row) end)
+    end)
+    |> length()
+  end
+
+  defp chunk_contiguous(ns) do
+    [n1 | ns] = Enum.sort(ns)
+    Enum.chunk_while(ns, [n1], &chunker/2, &chunker/1)
+  end
+
+  defp chunker(n, chunk) when abs(n - hd(chunk)) == 1, do: {:cont, [n | chunk]}
+  defp chunker(n, chunk), do: {:cont, Enum.reverse(chunk), [n]}
+  defp chunker(chunk), do: {:cont, Enum.reverse(chunk), nil}
+
+  defp subtract({x1, y1}, {x2, y2}), do: {x1 - x2, y1 - y2}
 end
--- test/advent_of_code/solution/year_2024/day_12_test.exs
+++ test/advent_of_code/solution/year_2024/day_12_test.exs
@@ -5,22 +5,60 @@ defmodule AdventOfCode.Solution.Year2024.Day12Test do
 
   setup do
     [
-      input: """
+      input1: """
+      AAAA
+      BBCD
+      BBCC
+      EEEC
+      """,
+      input2: """
+      OOOOO
+      OXOXO
+      OOOOO
+      OXOXO
+      OOOOO
+      """,
+      input3: """
+      RRRRIICCFF
+      RRRRIICCCF
+      VVRRRCCFFF
+      VVRCCCJFFF
+      VVVVCJJCFE
+      VVIVCCJJEE
+      VVIIICJJEE
+      MIIIIIJJEE
+      MIIISIJEEE
+      MMMISSJEEE
+      """,
+      input4: """
+      EEEEE
+      EXXXX
+      EEEEE
+      EXXXX
+      EEEEE
+      """,
+      input5: """
+      AAAAAA
+      AAABBA
+      AAABBA
+      ABBAAA
+      ABBAAA
+      AAAAAA
       """
     ]
   end
 
-  @tag :skip
-  test "part1", %{input: input} do
-    result = part1(input)
-
-    assert result
+  test "part1", ctx do
+    assert ctx.input1 |> parse() |> part1() == 140
+    assert ctx.input2 |> parse() |> part1() == 772
+    assert ctx.input3 |> parse() |> part1() == 1930
   end
 
-  @tag :skip
-  test "part2", %{input: input} do
-    result = part2(input)
-
-    assert result
+  test "part2", ctx do
+    assert ctx.input1 |> parse() |> part2() == 80
+    assert ctx.input2 |> parse() |> part2() == 436
+    assert ctx.input3 |> parse() |> part2() == 1206
+    assert ctx.input4 |> parse() |> part2() == 236
+    assert ctx.input5 |> parse() |> part2() == 368
   end
 end
