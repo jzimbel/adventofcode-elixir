3aff998a486dfb84e8fc64fc3c67cedb4efa2a0a
Jon Zimbel <jzimbel@gmail.com>
2024-12-15 00:13:48 -0500

    Day 13 solution and tests

--- lib/advent_of_code/solution/year_2024/day_13.ex
+++ lib/advent_of_code/solution/year_2024/day_13.ex
@@ -1,7 +1,50 @@
 defmodule AdventOfCode.Solution.Year2024.Day13 do
-  def part1(_input) do
+  use AdventOfCode.Solution.SharedParse
+
+  @impl true
+  def parse(input) do
+    for spec <- String.split(input, "\n\n", trim: true) do
+      List.to_tuple(
+        for(
+          binding <- Regex.run(spec_pat(), spec, capture: :all_but_first),
+          do: String.to_integer(binding)
+        )
+      )
+    end
   end
 
-  def part2(_input) do
+  defp spec_pat do
+    ~r"""
+    Button A: X\+(\d+), Y\+(\d+)
+    Button B: X\+(\d+), Y\+(\d+)
+    Prize: X=(\d+), Y=(\d+)\
+    """
+  end
+
+  def part1(specs) do
+    for spec <- specs, t = min_tokens(spec), t != nil, reduce: 0 do
+      acc -> acc + t
+    end
+  end
+
+  def part2(specs) do
+    part1(for spec <- specs, do: embiggen(spec))
+  end
+
+  defp embiggen({ax, ay, bx, by, x, y}) do
+    {ax, ay, bx, by, x + 10_000_000_000_000, y + 10_000_000_000_000}
+  end
+
+  defp min_tokens({ax, ay, bx, by, x, y}) do
+    # Assumes all inputs are positive and denominator exprs don't evalute to 0.
+    # I can't believe this puzzle got me to solve a system of equations by hand.
+    with 0 <- rem(ax * y - ay * x, ax * by - ay * bx),
+         b_presses = div(ax * y - ay * x, ax * by - ay * bx),
+         0 <- rem(x - bx * b_presses, ax),
+         a_presses = div(x - bx * b_presses, ax) do
+      a_presses * 3 + b_presses
+    else
+      _ -> nil
+    end
   end
 end
--- test/advent_of_code/solution/year_2024/day_13_test.exs
+++ test/advent_of_code/solution/year_2024/day_13_test.exs
@@ -6,21 +6,36 @@ defmodule AdventOfCode.Solution.Year2024.Day13Test do
   setup do
     [
       input: """
+      Button A: X+94, Y+34
+      Button B: X+22, Y+67
+      Prize: X=8400, Y=5400
+
+      Button A: X+26, Y+66
+      Button B: X+67, Y+21
+      Prize: X=12748, Y=12176
+
+      Button A: X+17, Y+86
+      Button B: X+84, Y+37
+      Prize: X=7870, Y=6450
+
+      Button A: X+69, Y+23
+      Button B: X+27, Y+71
+      Prize: X=18641, Y=10279
       """
     ]
   end
 
-  @tag :skip
   test "part1", %{input: input} do
-    result = part1(input)
+    result = input |> parse() |> part1()
 
-    assert result
+    assert result == 480
   end
 
-  @tag :skip
   test "part2", %{input: input} do
-    result = part2(input)
-
-    assert result
+    [spec1, spec2, spec3, spec4] = String.split(input, "\n\n", trim: true)
+    assert spec1 |> parse() |> part2() == 0
+    assert spec2 |> parse() |> part2() > 0
+    assert spec3 |> parse() |> part2() == 0
+    assert spec4 |> parse() |> part2() > 0
   end
 end
