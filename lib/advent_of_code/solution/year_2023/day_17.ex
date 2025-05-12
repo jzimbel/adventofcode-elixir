cb05733ea8d161ce5451f16dcef2a2d9c21af1eb
Jon Zimbel <jzimbel@gmail.com>
2025-05-11 21:43:39 -0400

    Remove duplicate `AdventOfCode.Algo` module

--- lib/advent_of_code/solution/year_2023/day_17.ex
+++ lib/advent_of_code/solution/year_2023/day_17.ex
@@ -1,10 +1,3 @@
-# TODO: Move to its own module
-defmodule AdventOfCode.Algo do
-  @moduledoc """
-  Algorithms for pathfinding and such.
-  """
-end
-
 defmodule AdventOfCode.Solution.Year2023.Day17 do
   def part1(_input) do
   end
