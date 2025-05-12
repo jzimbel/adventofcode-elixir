ab69bca6bcf1ece12ad6b59842482a50fb6ff280
Jon Zimbel <jzimbel@gmail.com>
2025-05-11 21:44:07 -0400

    Day 18 solution and tests

--- lib/advent_of_code/solution/year_2024/day_18.ex
+++ lib/advent_of_code/solution/year_2024/day_18.ex
@@ -1,7 +1,60 @@
 defmodule AdventOfCode.Solution.Year2024.Day18 do
-  def part1(_input) do
+  alias AdventOfCode.Algo
+  alias AdventOfCode.Grid, as: G
+
+  use AdventOfCode.Solution.SharedParse
+
+  @impl true
+  def parse(input, grid_size \\ 71) do
+    grid = G.new(grid_size, grid_size)
+
+    drops =
+      for line <- String.split(input, "\n", trim: true) do
+        [x, y] = for n <- String.split(line, ","), do: String.to_integer(n)
+        {x, y}
+      end
+
+    {grid, drops}
   end
 
-  def part2(_input) do
+  def part1({grid, drops}, t \\ 1024) do
+    grid =
+      drops
+      |> Enum.take(t)
+      |> Enum.reduce(grid, &G.replace(&2, &1, ?#))
+
+    {:ok, path} = Algo.a_star(grid, {0, 0}, {grid.width - 1, grid.height - 1})
+    steps = length(path) - 1
+    steps
+  end
+
+  def part2({grid, drops}) do
+    {x, y} = find_blocker(grid, drops)
+    "#{x},#{y}"
+  end
+
+  defp find_blocker(grid, drops, prev_path \\ nil, prev_drop \\ nil)
+
+  defp find_blocker(grid, [drop | drops], prev_path, prev_drop) do
+    if is_nil(prev_path) or Enum.any?(prev_path, &(G.at(grid, &1) == ?#)) do
+      case Algo.a_star(grid, {0, 0}, {grid.width - 1, grid.height - 1}) do
+        {:ok, path} -> find_blocker(G.replace(grid, drop, ?#), drops, path, drop)
+        :error -> prev_drop
+      end
+    else
+      find_blocker(G.replace(grid, drop, ?#), drops, prev_path, drop)
+    end
+  end
+
+  # No drops left to apply, but we haven't checked whether the final drop blocked the path
+  defp find_blocker(grid, [], prev_path, prev_drop) do
+    if is_nil(prev_path) or Enum.any?(prev_path, &(G.at(grid, &1) == ?#)) do
+      case Algo.a_star(grid, {0, 0}, {grid.width - 1, grid.height - 1}) do
+        {:ok, _path} -> :error
+        :error -> prev_drop
+      end
+    else
+      :error
+    end
   end
 end
--- test/advent_of_code/solution/year_2024/day_18_test.exs
+++ test/advent_of_code/solution/year_2024/day_18_test.exs
@@ -6,21 +6,44 @@ defmodule AdventOfCode.Solution.Year2024.Day18Test do
   setup do
     [
       input: """
+      5,4
+      4,2
+      4,5
+      3,0
+      2,1
+      6,3
+      2,4
+      1,5
+      0,6
+      3,3
+      2,6
+      5,1
+      1,2
+      5,5
+      2,5
+      6,5
+      1,4
+      0,4
+      6,4
+      1,1
+      6,1
+      1,0
+      0,5
+      1,6
+      2,0
       """
     ]
   end
 
-  @tag :skip
   test "part1", %{input: input} do
-    result = part1(input)
+    result = input |> parse(7) |> part1(12)
 
-    assert result
+    assert result == 22
   end
 
-  @tag :skip
   test "part2", %{input: input} do
-    result = part2(input)
+    result = input |> parse(7) |> part2()
 
-    assert result
+    assert result == "6,1"
   end
 end
