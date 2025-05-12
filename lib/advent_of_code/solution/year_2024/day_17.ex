5f75f151eec32b593a3a833e3e5959ac20946c02
Jon Zimbel <jzimbel@gmail.com>
2025-05-11 21:36:56 -0400

    Day 17 solution and tests

--- lib/advent_of_code/solution/year_2024/day_17.ex
+++ lib/advent_of_code/solution/year_2024/day_17.ex
@@ -1,7 +1,87 @@
 defmodule AdventOfCode.Solution.Year2024.Day17 do
-  def part1(_input) do
+  require Bitwise
+
+  use AdventOfCode.Solution.SharedParse
+
+  @enforce_keys [:a]
+  defstruct [:a, b: 0, c: 0, ptr: 0]
+
+  @impl true
+  def parse(input) do
+    [a, program] =
+      ~r"Register A: (\d+).*Program: ([,\d]+)"s
+      |> Regex.run(input, capture: :all_but_first)
+
+    program =
+      for {n, i} <- Enum.with_index(String.split(program, ",")), into: %{} do
+        {i, String.to_integer(n)}
+      end
+
+    {%__MODULE__{a: String.to_integer(a)}, program}
   end
 
-  def part2(_input) do
+  def part1({init_state, program}), do: stream_output(init_state, program) |> Enum.join(",")
+  def part2({_init_state, program}), do: gen_a(program) |> Enum.at(0)
+
+  defp gen_a(program) do
+    (map_size(program) - 1)..0//-1
+    |> Enum.reduce([0], fn i, stream ->
+      n = program[i]
+
+      stream
+      |> Stream.map(&Bitwise.bsl(&1, 3))
+      |> Stream.flat_map(fn shifted_partial_a ->
+        0..7
+        |> Stream.flat_map(fn octet ->
+          a = shifted_partial_a + octet
+          if first_output(a, program) == n, do: [a], else: []
+        end)
+      end)
+    end)
   end
+
+  defp first_output(a, program) do
+    Enum.at(stream_output(%__MODULE__{a: a}, program), 0)
+  end
+
+  defp stream_output(init_state, program) do
+    Stream.resource(fn -> init_state end, &next(&1, program), fn _final_state -> nil end)
+  end
+
+  defp next(state, program) when is_map_key(program, state.ptr) do
+    opcode = Map.fetch!(program, state.ptr)
+    arg = program |> Map.fetch!(state.ptr + 1) |> resolve_arg(opcode, state)
+    op(opcode, arg, state)
+  end
+
+  defp next(state, _program), do: {:halt, state}
+
+  defp op(opcode, n, state) do
+    case opcode do
+      0 -> {[], inc_ptr(put_in(state.a, div(state.a, 2 ** n)))}
+      1 -> {[], inc_ptr(put_in(state.b, Bitwise.bxor(state.b, n)))}
+      2 -> {[], inc_ptr(put_in(state.b, rem(n, 8)))}
+      3 -> {[], if(state.a != 0, do: put_in(state.ptr, n), else: inc_ptr(state))}
+      4 -> {[], inc_ptr(put_in(state.b, Bitwise.bxor(state.b, state.c)))}
+      5 -> {[rem(n, 8)], inc_ptr(state)}
+      6 -> {[], inc_ptr(put_in(state.b, div(state.a, 2 ** n)))}
+      7 -> {[], inc_ptr(put_in(state.c, div(state.a, 2 ** n)))}
+    end
+  end
+
+  defp resolve_arg(arg, opcode, _state)
+       when opcode in [1, 3, 4]
+       when arg not in 4..7,
+       do: arg
+
+  defp resolve_arg(arg, _combo_opcode, state) do
+    case arg do
+      4 -> state.a
+      5 -> state.b
+      6 -> state.c
+      7 -> raise "combo arg 7 not implemented"
+    end
+  end
+
+  defp inc_ptr(state), do: put_in(state.ptr, state.ptr + 2)
 end
--- test/advent_of_code/solution/year_2024/day_17_test.exs
+++ test/advent_of_code/solution/year_2024/day_17_test.exs
@@ -3,24 +3,49 @@ defmodule AdventOfCode.Solution.Year2024.Day17Test do
 
   import AdventOfCode.Solution.Year2024.Day17
 
-  setup do
-    [
-      input: """
-      """
-    ]
+  defp make_input(a, program) do
+    """
+    Register A: #{a}
+    Register B: 0
+    Register C: 0
+
+    Program: #{program}
+    """
   end
 
-  @tag :skip
-  test "part1", %{input: input} do
-    result = part1(input)
+  test "part1" do
+    input = make_input(729, "0,1,5,4,3,0")
+    result = input |> parse() |> part1()
 
-    assert result
+    assert result == "4,6,3,5,6,3,5,2,1,0"
   end
 
-  @tag :skip
-  test "part2", %{input: input} do
-    result = part2(input)
+  test "part2" do
+    # Part 2 solution is specific to the input program, which has the following characteristics:
+    # - Prints once per "loop"
+    # - Each iteration of the loop bit-shifts A right by 3 bits
+    #
+    # The test program here needs to have the same characteristics.
+    # This one does the following:
+    # 2,4 - b <- a<<3
+    # 1,5 - b <- xor 0b101  # This was necessary to make quine_a produce the final 0, idk why
+    # 5,5 - OUT <- b % 8
+    # 0,3 - a <- a<<3
+    # 3,0 - JANZ 0
+    program = "2,4,1,5,5,5,0,3,3,0"
 
-    assert result
+    input = make_input(0, program)
+    quine_a = input |> parse() |> part2()
+
+    # quine_a should be composed of 10 octets,
+    # one for each number in the program.
+    assert String.length(Integer.to_string(quine_a, 8)) == 10
+
+    successive_outputs =
+      program
+      |> Stream.iterate(&(make_input(quine_a, &1) |> parse() |> part1()))
+      |> Enum.take(10)
+
+    assert Enum.all?(successive_outputs, &(&1 == program))
   end
 end
