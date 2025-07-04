3be54197798809b46e1f95cbdfc07346d84596b0
Jon Zimbel <jzimbel@gmail.com>
2025-07-03 23:39:11 -0400

    Day 20 solution and tests

--- lib/advent_of_code/solution/year_2024/day_20.ex
+++ lib/advent_of_code/solution/year_2024/day_20.ex
@@ -1,7 +1,37 @@
 defmodule AdventOfCode.Solution.Year2024.Day20 do
-  def part1(_input) do
+  alias AdventOfCode.Grid, as: G
+  import AdventOfCode.Algo.Helpers, only: [manhattan_distance: 2]
+
+  use AdventOfCode.Solution.SharedParse
+
+  @impl true
+  def parse(input) do
+    grid = G.from_input(input)
+    {start, ?S} = Enum.find(grid, &match?({_, ?S}, &1))
+    {goal, ?E} = Enum.find(grid, &match?({_, ?E}, &1))
+    grid = G.replace(grid, %{start => ?., goal => ?.})
+
+    {grid, start, goal}
   end
 
-  def part2(_input) do
+  def part1(race_course), do: race_course |> solve(2) |> Enum.count(&(&1 >= 100))
+  def part2(race_course), do: race_course |> solve(20) |> Enum.count(&(&1 >= 100))
+
+  def solve({grid, start, goal}, max_jump) do
+    # NB: the original path visits all empty cells in the grid, so any valid cheat will end up back on the path.
+    {:ok, {path, cost}} = AdventOfCode.Algo.a_star_one(grid, start, goal)
+    cheat_savings(Enum.with_index(path), max_jump, cost, [])
+  end
+
+  defp cheat_savings([], _, _, acc), do: acc
+
+  defp cheat_savings([{coords, cost_up_to_jump} | path], max_jump, total_cost, acc) do
+    savings =
+      for {other_coords, cost_up_to_other_coords} <- path,
+          Enum.member?(2..max_jump//1, jump_dist = manhattan_distance(coords, other_coords)),
+          0 < (saved = cost_up_to_other_coords - cost_up_to_jump - jump_dist),
+          do: saved
+
+    cheat_savings(path, max_jump, total_cost, savings ++ acc)
   end
 end
--- test/advent_of_code/solution/year_2024/day_20_test.exs
+++ test/advent_of_code/solution/year_2024/day_20_test.exs
@@ -6,21 +6,75 @@ defmodule AdventOfCode.Solution.Year2024.Day20Test do
   setup do
     [
       input: """
+      ###############
+      #...#...#.....#
+      #.#.#.#.#.###.#
+      #S#...#.#.#...#
+      #######.#.#.###
+      #######.#.#...#
+      #######.#.###.#
+      ###..E#...#...#
+      ###.#######.###
+      #...###...#...#
+      #.#####.#.###.#
+      #.#...#.#.#...#
+      #.#.#.#.#.#.###
+      #...#...#...###
+      ###############
       """
     ]
   end
 
-  @tag :skip
   test "part1", %{input: input} do
-    result = part1(input)
+    expected_freqs =
+      %{
+        2 => 14,
+        4 => 14,
+        6 => 2,
+        8 => 4,
+        10 => 2,
+        12 => 3,
+        20 => 1,
+        36 => 1,
+        38 => 1,
+        40 => 1,
+        64 => 1
+      }
 
-    assert result
+    result =
+      input
+      |> parse()
+      |> solve(2)
+      |> Enum.frequencies()
+
+    assert result == expected_freqs
   end
 
-  @tag :skip
   test "part2", %{input: input} do
-    result = part2(input)
+    expected_freqs = %{
+      50 => 32,
+      52 => 31,
+      54 => 29,
+      56 => 39,
+      58 => 25,
+      60 => 23,
+      62 => 20,
+      64 => 19,
+      66 => 12,
+      68 => 14,
+      70 => 12,
+      72 => 22,
+      74 => 4,
+      76 => 3
+    }
 
-    assert result
+    result =
+      input
+      |> parse()
+      |> solve(20)
+      |> Enum.frequencies()
+      |> Map.filter(fn {k, _} -> k >= 50 end)
+
+    assert result == expected_freqs
   end
 end
