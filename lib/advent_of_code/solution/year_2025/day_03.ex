98e16538dbf39b5303e736ef5432a571a6d12777
Jon Zimbel <jzimbel+github@gmail.com>
2025-12-03 23:50:08 -0500

    Day 3 solution and tests

--- lib/advent_of_code/solution/year_2025/day_03.ex
+++ lib/advent_of_code/solution/year_2025/day_03.ex
@@ -1,7 +1,37 @@
 defmodule AdventOfCode.Solution.Year2025.Day03 do
-  def part1(_input) do
+  use AdventOfCode.Solution.SharedParse
+
+  @impl true
+  def parse(input) do
+    input
+    |> String.split()
+    |> Enum.map(&for(d <- String.to_charlist(&1), do: d - ?0))
   end
 
-  def part2(_input) do
+  def part1(battery_banks), do: Enum.sum_by(battery_banks, &max_jolt(&1, 2))
+  def part2(battery_banks), do: Enum.sum_by(battery_banks, &max_jolt(&1, 12))
+
+  defp max_jolt(batteries, num_to_activate) do
+    bank_size = length(batteries)
+    init_active_group = List.duplicate(0, num_to_activate)
+
+    batteries
+    # Annotate batteries with the earliest index they can occupy in the activated group
+    |> Enum.with_index(fn b, i -> {b, max(num_to_activate + i - bank_size, 0)} end)
+    |> Enum.reduce(init_active_group, &update_active_group/2)
+    |> Integer.undigits()
   end
+
+  defp update_active_group({b, min_i}, active_group) do
+    {ineligible, eligible} = Enum.split(active_group, min_i)
+    update_active_group(b, eligible, Enum.reverse(ineligible))
+  end
+
+  defp update_active_group(_b, [], acc), do: Enum.reverse(acc)
+
+  defp update_active_group(b, [active | actives], acc) when b > active,
+    do: update_active_group(b, [], List.duplicate(0, length(actives)) ++ [b | acc])
+
+  defp update_active_group(b, [active | actives], acc),
+    do: update_active_group(b, actives, [active | acc])
 end
--- test/advent_of_code/solution/year_2025/day_03_test.exs
+++ test/advent_of_code/solution/year_2025/day_03_test.exs
@@ -6,21 +6,23 @@ defmodule AdventOfCode.Solution.Year2025.Day03Test do
   setup do
     [
       input: """
+      987654321111111
+      811111111111119
+      234234234234278
+      818181911112111
       """
     ]
   end
 
-  @tag :skip
   test "part1", %{input: input} do
-    result = part1(input)
+    result = input |> parse() |> part1()
 
-    assert result
+    assert result == 357
   end
 
-  @tag :skip
   test "part2", %{input: input} do
-    result = part2(input)
+    result = input |> parse() |> part2()
 
-    assert result
+    assert result == 3_121_910_778_619
   end
 end
