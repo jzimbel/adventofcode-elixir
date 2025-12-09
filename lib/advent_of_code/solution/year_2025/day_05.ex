40a6fc2bebc08b9e3d04e6b32f67e0f2e87226e1
Jon Zimbel <jzimbel+github@gmail.com>
2025-12-08 23:13:04 -0500

    Day 5 solution and tests

--- lib/advent_of_code/solution/year_2025/day_05.ex
+++ lib/advent_of_code/solution/year_2025/day_05.ex
@@ -1,7 +1,44 @@
 defmodule AdventOfCode.Solution.Year2025.Day05 do
-  def part1(_input) do
+  use AdventOfCode.Solution.SharedParse
+
+  @impl true
+  def parse(input) do
+    [ranges, ids] = String.split(input, "\n\n")
+    {parse_and_consolidate_ranges(ranges), parse_ids(ids)}
   end
 
-  def part2(_input) do
+  def part1({ranges, ids}),
+    do: Enum.count(ids, fn id -> Enum.any?(ranges, &in_range_fast?(&1, id)) end)
+
+  def part2({ranges, _ids}), do: Enum.sum_by(ranges, &Range.size/1)
+
+  defp consolidate(ranges) do
+    [first_range | ranges] = Enum.sort_by(ranges, & &1.first)
+
+    Enum.chunk_while(
+      ranges,
+      first_range,
+      fn r2, r1 ->
+        if Range.disjoint?(r1, r2),
+          do: {:cont, r1, r2},
+          else: {:cont, r1.first..max(r1.last, r2.last)//1}
+      end,
+      fn final_range -> {:cont, final_range, nil} end
+    )
   end
+
+  defp parse_and_consolidate_ranges(str) do
+    str
+    |> String.split()
+    |> Enum.map(fn line ->
+      [first, last] = String.split(line, "-")
+      String.to_integer(first)..String.to_integer(last)//1
+    end)
+    |> consolidate()
+  end
+
+  defp parse_ids(str), do: Enum.map(String.split(str), &String.to_integer/1)
+
+  # About 3x faster than `n in range` because that logic is generalized for ranges with any step
+  defp in_range_fast?(first..last//1, n), do: n >= first and n <= last
 end
--- test/advent_of_code/solution/year_2025/day_05_test.exs
+++ test/advent_of_code/solution/year_2025/day_05_test.exs
@@ -6,21 +6,30 @@ defmodule AdventOfCode.Solution.Year2025.Day05Test do
   setup do
     [
       input: """
+      3-5
+      10-14
+      16-20
+      12-18
+
+      1
+      5
+      8
+      11
+      17
+      32
       """
     ]
   end
 
-  @tag :skip
   test "part1", %{input: input} do
-    result = part1(input)
+    result = input |> parse() |> part1()
 
-    assert result
+    assert result == 3
   end
 
-  @tag :skip
   test "part2", %{input: input} do
-    result = part2(input)
+    result = input |> parse() |> part2()
 
-    assert result
+    assert result == 14
   end
 end
